c3ec02732ae8c50651119b1144fd0d3517e773bc
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-13 19:48:08 +0900

    feat - 크롤링 테스트

--- src/main/java/com/example/perfume/PerfumeApplication.java
+++ src/main/java/com/example/perfume/PerfumeApplication.java
@@ -1,13 +1,27 @@
 package com.example.perfume;
 
+import com.example.perfume.crawling.Clawling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+/*
 @SpringBootApplication
 public class PerfumeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerfumeApplication.class, args);
+	}
+
+}
+*/
+
+public class PerfumeApplication {
+
+	public static void main(String[] args) throws IOException {
+		Clawling clawling = new Clawling();
+
+		clawling.CrawWebSource();
 	}
 
 }
--- src/main/java/com/example/perfume/crawling/Clawling.java
+++ src/main/java/com/example/perfume/crawling/Clawling.java
@@ -1,6 +1,30 @@
 package com.example.perfume.crawling;
 
+
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
 public class Clawling {
+    private static final String URL = "https://perfumegraphy.com/category/citrus/674/";
+
+    public Document connectAndGetDocument() throws IOException {
+        Connection connection = Jsoup.connect(URL);
+        Document document = connection.get();
+
+        return document;
+    }
+
+    public void CrawWebSource() throws IOException {
+        Elements urlElements = connectAndGetDocument().getElementsByClass("thumbnail");
+        for(Element element : urlElements){
+            System.out.println(element);
+        }
+    }
+
 }
