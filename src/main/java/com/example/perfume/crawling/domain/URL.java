29637b44257d2239cd918477d3ba1543848eadb0
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-14 14:21:39 +0900

    feat - 미안..머리가 안돌아가서 그냥 수작업했어..

---
+++ src/main/java/com/example/perfume/crawling/domain/URL.java
@@ -0,0 +1,53 @@
+package com.example.perfume.crawling.domain;
+
+public class URL {
+    public static String CITRUS_URL = "https://perfumegraphy.com/category/citrus/674/";
+    public static String CITRUS_URL2="https://perfumegraphy.com/category/citrus/674/?page=2";
+    public static String CITRUS_URL3="https://perfumegraphy.com/category/citrus/674/?page=3";
+    public static String CITRUS_URL4="https://perfumegraphy.com/category/citrus/674/?page=4";
+    public static String CITRUS_URL5="https://perfumegraphy.com/category/citrus/674/?page=5";
+
+    public static String SOAPY_URL = "https://perfumegraphy.com/category/citrus/675/";
+    public static String SOAPY_URL2 = "https://perfumegraphy.com/category/citrus/675/?page=2";
+    public static String SOAPY_URL3 = "https://perfumegraphy.com/category/citrus/675/?page=3";
+    public static String SOAPY_URL4 = "https://perfumegraphy.com/category/citrus/675/?page=4";
+    public static String SOAPY_URL5 = "https://perfumegraphy.com/category/citrus/675/?page=5";
+
+    public static String FLORAL_URL = "https://perfumegraphy.com/category/citrus/676/";
+    public static String FLORAL_URL2 = "https://perfumegraphy.com/category/citrus/676/?page=2";
+    public static String FLORAL_URL3 = "https://perfumegraphy.com/category/citrus/676/?page=3";
+    public static String FLORAL_URL4 = "https://perfumegraphy.com/category/citrus/676/?page=4";
+    public static String FLORAL_URL5 = "https://perfumegraphy.com/category/citrus/676/?page=5";
+
+    public static String FRUITY_URL = "https://perfumegraphy.com/category/citrus/677/";
+    public static String FRUITY_URL2 = "https://perfumegraphy.com/category/citrus/677/?page=2";
+    public static String FRUITY_URL3 = "https://perfumegraphy.com/category/citrus/677/?page=3";
+    public static String FRUITY_URL4 = "https://perfumegraphy.com/category/citrus/677/?page=4";
+    public static String FRUITY_URL5 = "https://perfumegraphy.com/category/citrus/677/?page=5";
+
+    public static String WOODY_URL = "https://perfumegraphy.com/category/citrus/678/";
+    public static String WOODY_URL2 = "https://perfumegraphy.com/category/citrus/678/?page=2";
+    public static String WOODY_URL3 = "https://perfumegraphy.com/category/citrus/678/?page=3";
+    public static String WOODY_URL4 = "https://perfumegraphy.com/category/citrus/678/?page=4";
+    public static String WOODY_URL5 = "https://perfumegraphy.com/category/citrus/678/?page=5";
+
+    public static String SPICY_URL = "https://perfumegraphy.com/category/citrus/679/";
+    public static String SPICY_URL2 = "https://perfumegraphy.com/category/citrus/679/?page=2";
+    public static String SPICY_URL3 = "https://perfumegraphy.com/category/citrus/679/?page=3";
+    public static String SPICY_URL4 = "https://perfumegraphy.com/category/citrus/679/?page=4";
+    public static String SPICY_URL5 = "https://perfumegraphy.com/category/citrus/679/?page=5";
+
+    public static String VANILLA_URL = "https://perfumegraphy.com/category/citrus/680/";
+    public static String VANILLA_URL2 = "https://perfumegraphy.com/category/citrus/680/?page=2";
+    public static String VANILLA_URL3 = "https://perfumegraphy.com/category/citrus/680/?page=3";
+    public static String VANILLA_URL4 = "https://perfumegraphy.com/category/citrus/680/?page=4";
+    public static String VANILLA_URL5 = "https://perfumegraphy.com/category/citrus/680/?page=5";
+
+    public static String POWDERY_URL = "https://perfumegraphy.com/category/citrus/681/";
+    public static String POWDERY_URL2 = "https://perfumegraphy.com/category/citrus/681/?page=2";
+    public static String POWDERY_URL3 = "https://perfumegraphy.com/category/citrus/681/?page=3";
+    public static String POWDERY_URL4 = "https://perfumegraphy.com/category/citrus/681/?page=4";
+    public static String POWDERY_URL5 = "https://perfumegraphy.com/category/citrus/681/?page=5";
+
+
+}
