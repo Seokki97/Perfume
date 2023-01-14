1d26609328e0a54fe1f76179b64cd6e64a43df83
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-14 14:20:36 +0900

    refactor - URL변경 용이하기위해 매개변수로 수정

--- src/main/java/com/example/perfume/crawling/service/ClawlingService.java
+++ src/main/java/com/example/perfume/crawling/service/ClawlingService.java
@@ -2,6 +2,7 @@ package com.example.perfume.crawling.service;
 
 import com.example.perfume.crawling.domain.Feature;
 import com.example.perfume.crawling.domain.Perfume;
+import com.example.perfume.crawling.domain.URL;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,19 +13,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClawlingService {
-    private static final String URL = "https://perfumegraphy.com/category/citrus/674/";
+
     private static final int ZERO = 0;
 
-    public Document connectAndGetDocument() throws IOException {
-        Connection connection = Jsoup.connect(URL);
+    public Document connectAndGetDocument(String url) throws IOException {
+        Connection connection = Jsoup.connect(url);
         Document document = connection.get();
 
         return document;
     }
 
 
-    public List<Perfume> crawPerfumeName() throws IOException {
-        Elements setRange = connectAndGetDocument().select("section.thumbnail a img");
+    public List<Perfume> crawPerfumeName(String url) throws IOException {
+        Elements setRange = connectAndGetDocument(url).select("section.thumbnail a img");
         List<Perfume> perfumeNameList = new ArrayList<>();
         for (int firstIndexOfList = ZERO; firstIndexOfList < setRange.size(); firstIndexOfList++) {
             Perfume perfume = new Perfume(setRange.get(firstIndexOfList).attr("alt"));
@@ -33,8 +34,8 @@ public class ClawlingService {
         return perfumeNameList;
     }
 
-    public List<Feature> crawPerfumeFeature() throws IOException {
-        Elements setRange = connectAndGetDocument().select("ul.spec li.summary");
+    public List<Feature> crawPerfumeFeature(String url) throws IOException {
+        Elements setRange = connectAndGetDocument(url).select("ul.spec li.summary");
 
         List<Feature> featureList = new ArrayList<>();
         for (int firstIndexOfList = ZERO; firstIndexOfList < setRange.size(); firstIndexOfList++) {
