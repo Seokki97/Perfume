c6037314c1558c8b39e02ea845691c1913d0c45c
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-23 17:32:21 +0900

    refactor - 필드 수정

--- src/main/java/com/example/perfume/crawling/service/CsvFileService.java
+++ src/main/java/com/example/perfume/crawling/service/CsvFileService.java
@@ -6,18 +6,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvFileService {
-     private String filePath = "C:/Users/wnstj/perfume/Perfume1.csv";
+    private String filePath = "C:/Users/wnstj/perfume/Perfume3.csv";
 
-        File importedFile = new File(filePath);
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(importedFile));
+    private File importedFile;
+    private BufferedWriter bufferedWriter;
 
     public CsvFileService() throws IOException {
+        this.importedFile = new File(filePath);
+        this.bufferedWriter = new BufferedWriter(new FileWriter(importedFile));
     }
 
 
-    public void makeCsvFile(CrawlingService crawlingService, String targetUrl) throws IOException {
-
-        for (int i = 0; i < crawlingService.crawPerfumeFeature(targetUrl).size(); i++) {
+    public void makeCsvData(CrawlingService crawlingService, String targetUrl) throws IOException {
+        int listSize = crawlingService.crawPerfumeFeature(targetUrl).size();
+        for (int i = 0; i < listSize; i++) {
             String crawledData = "";
             crawledData = crawlingService.crawPerfumeName(targetUrl).get(i) + "," + crawlingService.crawPerfumeFeature(targetUrl).get(i);
             bufferedWriter.write(crawledData);
@@ -25,11 +27,12 @@ public class CsvFileService {
         }
         bufferedWriter.flush();
     }
+
     public void bufferClose() throws IOException {
         bufferedWriter.close();
     }
 
-    public void makeNewLine() throws IOException{
+    public void makeNewLine() throws IOException {
         bufferedWriter.newLine();
     }
 }
