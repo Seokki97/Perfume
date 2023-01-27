87942cc6f687802d3fb7be9bcada9d545fba13aa
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 13:50:30 +0900

    feat - FeatureDto entity create

--- src/main/java/com/example/perfume/perfume/dto/FeatureDto.java
+++ src/main/java/com/example/perfume/perfume/dto/FeatureDto.java
@@ -1,4 +1,24 @@
 package com.example.perfume.perfume.dto;
 
+import com.example.perfume.perfume.domain.Feature;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "feature")
 public class FeatureDto {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String firstFeature;
+    private String secondFeature;
+    private String thirdFeature;
+    private String fourthFeature;
+
+
 }
