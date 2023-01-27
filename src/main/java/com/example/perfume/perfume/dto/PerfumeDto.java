7651862c2b08659befc996de29f51cbae846b923
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 13:58:20 +0900

    feat - create PerfumeDto

--- src/main/java/com/example/perfume/perfume/dto/PerfumeDto.java
+++ src/main/java/com/example/perfume/perfume/dto/PerfumeDto.java
@@ -1,5 +1,27 @@
 package com.example.perfume.perfume.dto;
 
 
+import com.example.perfume.perfume.domain.Feature;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Table;
+
+@Data
+@Table
 public class PerfumeDto {
+    private Long id;
+    private String perfumeName;
+    private String brandName;
+    private String perfumeFeature;
+    private Feature feature;
+
+    @Builder
+    public PerfumeDto(Long id, String perfumeName, String brandName, String perfumeFeature, Feature feature) {
+        this.id = id;
+        this.perfumeName = perfumeName;
+        this.brandName = brandName;
+        this.perfumeFeature = perfumeFeature;
+        this.feature = feature;
+    }
 }
