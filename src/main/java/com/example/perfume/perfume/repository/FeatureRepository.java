280949712df41834a7c46695a062d3b9bc8cb65f
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 14:01:07 +0900

    feat - create FeatureRepository

--- src/main/java/com/example/perfume/perfume/repository/FeatureRepository.java
+++ src/main/java/com/example/perfume/perfume/repository/FeatureRepository.java
@@ -2,6 +2,8 @@ package com.example.perfume.perfume.repository;
 
 import com.example.perfume.perfume.domain.Feature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeatureRepository extends JpaRepository<Feature,Long> {
 }
