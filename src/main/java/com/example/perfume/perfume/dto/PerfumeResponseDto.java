dc797380fbfc796f4c7ecee893be4b74180174a4
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 13:38:36 +0900

    feat - PerfumeResponseDto생성

--- src/main/java/com/example/perfume/perfume/dto/PerfumeResponseDto.java
+++ src/main/java/com/example/perfume/perfume/dto/PerfumeResponseDto.java
@@ -1,4 +1,33 @@
 package com.example.perfume.perfume.dto;
 
+import com.example.perfume.perfume.domain.Feature;
+import com.example.perfume.perfume.domain.Perfume;
+import lombok.Builder;
+
 public class PerfumeResponseDto {
+
+    private Long id;
+    private String perfumeName;
+    private String brandName;
+    private String perfumeFeature;
+    private Feature feature;
+
+    @Builder
+    public PerfumeResponseDto(Long id, String perfumeName, String brandName, String perfumeFeature, Feature feature) {
+        this.id = id;
+        this.perfumeName = perfumeName;
+        this.brandName = brandName;
+        this.perfumeFeature = perfumeFeature;
+        this.feature = feature;
+    }
+
+    public Perfume toEntity(Feature feature){
+        return Perfume.builder()
+                .id(id)
+                .perfumeName(perfumeName)
+                .brandName(brandName)
+                .perfumeFeature(perfumeFeature)
+                .feature(feature)
+                .build();
+    }
 }
