e9c670f28884f2ec1af8d4a0666605d61eaf0d1c
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 23:49:02 +0900

    refactor - 인코딩 버그 utf-8 반영

---
+++ src/main/java/com/example/perfume/perfume/service/CSVFileLoading.java
@@ -0,0 +1,56 @@
+package com.example.perfume.perfume.service;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVFileLoading {
+    String FILE_PATH = "C:/Users/wnstj/perfume/Perfume3.csv";
+
+    List<String> perfumeListTest = new ArrayList<>();
+
+
+    public InputStreamReader importFile() throws UnsupportedEncodingException, FileNotFoundException {
+
+        FileInputStream fileInputStream = new FileInputStream(FILE_PATH);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "utf-8");
+        return inputStreamReader;
+    }
+
+    public BufferedReader readPerfumeData() throws UnsupportedEncodingException, FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(importFile());
+
+        return bufferedReader;
+    }
+
+    public List<String> makePerfumeList(String[] array, String line) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            array = line.split(",");
+            list.add(array[i]);
+        }
+        return list;
+    }
+
+    public List<String> splitPerfumeData() throws IOException {
+        String line;
+        List<String> list = new ArrayList<>();
+
+        while ((line = readPerfumeData().readLine()) != null) {
+            String[] array = line.split(",");
+            list = makePerfumeList(array, line);
+            perfumeListTest = makePerfumeList(array, line);
+            System.out.println(perfumeListTest.get(0));
+        }
+        return list;
+    }
+
+    public void testPerfumeList() throws IOException {
+        splitPerfumeData();
+        for (int i = 0; i < perfumeListTest.size(); i++) {
+            System.out.println(perfumeListTest.get(i));
+
+        }
+    }
+
+}
