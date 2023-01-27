e207a4c5b4f282f2305944e8a4783f340c8f2082
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-27 13:51:42 +0900

    feat - FeatureRequestDto Dto create

--- src/main/java/com/example/perfume/perfume/dto/FeatureRequestDto.java
+++ src/main/java/com/example/perfume/perfume/dto/FeatureRequestDto.java
@@ -1,4 +1,37 @@
 package com.example.perfume.perfume.dto;
 
+import com.example.perfume.perfume.domain.Feature;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Table;
+
+@Data
+@Table(name = "feature")
 public class FeatureRequestDto {
+
+    private Long id;
+    private String firstFeature;
+    private String secondFeature;
+    private String thirdFeature;
+    private String fourthFeature;
+
+    @Builder
+    public FeatureRequestDto(Long id, String firstFeature, String secondFeature, String thirdFeature, String fourthFeature){
+        this.id = id;
+        this.firstFeature = firstFeature;
+        this.secondFeature = secondFeature;
+        this.thirdFeature = thirdFeature;
+        this.fourthFeature = fourthFeature;
+    }
+
+    public Feature toEntity(){
+        return Feature.builder()
+                .id(id)
+                .firstFeature(firstFeature)
+                .secondFeature(secondFeature)
+                .thirdFeature(thirdFeature)
+                .fourthFeature(fourthFeature)
+                .build();
+    }
 }
