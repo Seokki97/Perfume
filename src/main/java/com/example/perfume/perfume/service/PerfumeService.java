3fb05363a84e34109b0f9b2202b95565fcd63d0b
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-28 19:00:33 +0900

    feat - 향수 데이터 db저장 api 구현

---
+++ src/main/java/com/example/perfume/perfume/service/PerfumeService.java
@@ -0,0 +1,34 @@
+package com.example.perfume.perfume.service;
+
+
+import com.example.perfume.perfume.domain.Feature;
+import com.example.perfume.perfume.domain.Perfume;
+import com.example.perfume.perfume.dto.PerfumeDto;
+import com.example.perfume.perfume.repository.PerfumeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class PerfumeService {
+    private final PerfumeRepository perfumeRepository;
+
+    private final CSVFileLoading csvFileLoading;
+
+    public void savePerfumeData(Long id, PerfumeDto perfumeDto/*, Feature feature*/) throws IOException {
+        csvFileLoading.extractPerfumeName();
+        csvFileLoading.extractPerfumeFeature();
+        csvFileLoading.extractPerfumeBrand();
+        for (int i = 0; i < csvFileLoading.setMaxListSize(); i++) {
+            perfumeDto = new PerfumeDto(id,
+                    csvFileLoading.getPerfumeName().get(i),
+                    csvFileLoading.getPerfumeFeature().get(i),
+                    csvFileLoading.getPerfumeBrand().get(i)/*,
+                    feature*/);
+            Perfume perfumeDataSet = perfumeDto.toEntity(/*feature*/);
+            perfumeRepository.save(perfumeDataSet);
+        }
+    }
+}
--- src/test/java/com/example/perfume/PerfumeCrawlingApplicationTests.java
+++ src/test/java/com/example/perfume/PerfumeCrawlingApplicationTests.java
@@ -1,10 +1,11 @@
 package com.example.perfume;
 
+import com.example.perfume.perfume.service.CSVFileLoading;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PerfumeApplicationTests {
+class PerfumeCrawlingApplicationTests {
 
 	@Test
 	void contextLoads() {
