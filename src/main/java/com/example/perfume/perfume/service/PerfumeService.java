f2d96d36dd8b49cbd0069866c46df25b1226aeab
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-28 20:46:26 +0900

    feat - 조회, 삭제 api 생성

--- src/main/java/com/example/perfume/perfume/service/PerfumeService.java
+++ src/main/java/com/example/perfume/perfume/service/PerfumeService.java
@@ -1,7 +1,6 @@
 package com.example.perfume.perfume.service;
 
 
-import com.example.perfume.perfume.domain.Feature;
 import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.dto.PerfumeDto;
 import com.example.perfume.perfume.repository.PerfumeRepository;
@@ -20,6 +19,8 @@ public class PerfumeService {
     public void savePerfumeData(Long id, PerfumeDto perfumeDto/*, Feature feature*/) throws IOException {
         csvFileLoading.extractAllPerfumeData();
         for (int i = 0; i < csvFileLoading.setMaxListSize(); i++) {
+            /*,
+                    feature*/
             perfumeDto = new PerfumeDto(id,
                     csvFileLoading.getPerfumeName().get(i),
                     csvFileLoading.getPerfumeFeature().get(i),
@@ -28,5 +29,21 @@ public class PerfumeService {
             Perfume perfumeDataSet = perfumeDto.toEntity(/*feature*/);
             perfumeRepository.save(perfumeDataSet);
         }
+    }
+
+    public Perfume findPerfumeByName(String perfumeName) {
+        Perfume perfume = perfumeRepository.findByPerfumeName(perfumeName)
+                .orElseThrow(() -> new IllegalArgumentException("해당 향수를 찾을 수 없습니다."));
+        return perfume;
+    }
+
+    public Perfume findPerfumeByBrand(String brandName) {
+        Perfume perfume = perfumeRepository.findByBrandName(brandName)
+                .orElseThrow(() -> new IllegalArgumentException("해당 향수를 찾을 수 없습니다."));
+
+        return perfume;
+    }
+    public void deleteAllData(){
+        perfumeRepository.deleteAll();
     }
 }
