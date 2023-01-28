48f7eb63e78de7e4ac3ce7553dfe8f37ac9a4a9f
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-28 18:58:50 +0900

    feat - 향수 데이터를 이름,브랜드,특징별 필드 생성, 추출 메서드 구현

--- src/main/java/com/example/perfume/perfume/service/CSVFileLoading.java
+++ src/main/java/com/example/perfume/perfume/service/CSVFileLoading.java
@@ -1,19 +1,33 @@
 package com.example.perfume.perfume.service;
 
+import com.example.perfume.perfume.dto.PerfumeDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Service
+@Getter
 public class CSVFileLoading {
     private final String FILE_PATH = "C:/Users/wnstj/perfume/Perfume3.csv";
 
     private List<String> perfumeListTest;
+    private List<String> perfumeName;
+    private List<String> perfumeFeature;
+    private List<String> perfumeBrand;
     private BufferedReader bufferedReader;
 
 
-    public CSVFileLoading() throws FileNotFoundException, UnsupportedEncodingException {
+   public CSVFileLoading() throws FileNotFoundException, UnsupportedEncodingException {
         this.perfumeListTest = new ArrayList<>();
         this.bufferedReader = new BufferedReader(importFile());
+        this.perfumeName = new ArrayList<>();
+        this.perfumeBrand = new ArrayList<>();
+        this.perfumeFeature = new ArrayList<>();
     }
 
     public InputStreamReader importFile() throws UnsupportedEncodingException, FileNotFoundException {
@@ -25,12 +39,10 @@ public class CSVFileLoading {
 
 
     public List<String> makePerfumeList(String[] array) {
-
         for (int i = 0; i < array.length; i++) {
             perfumeListTest.add(array[i]);
 
         }
-
         return perfumeListTest;
     }
 
@@ -50,4 +62,34 @@ public class CSVFileLoading {
         }
     }
 
+    public List<String> extractPerfumeName() throws IOException {
+        splitPerfumeData();
+        for (int i = 0; i < perfumeListTest.size(); i += 3) {
+            perfumeName.add(perfumeListTest.get(i));
+        }
+        return perfumeName;
+    }
+
+    public List<String> extractPerfumeFeature() throws IOException {
+        splitPerfumeData();
+        for (int i = 1; i < perfumeListTest.size(); i += 3) {
+            perfumeFeature.add(perfumeListTest.get(i));
+        }
+        return perfumeFeature;
+
+    }
+
+    public List<String> extractPerfumeBrand() throws IOException {
+        splitPerfumeData();
+        for (int i = 2; i < perfumeListTest.size(); i += 3) {
+            perfumeBrand.add(perfumeListTest.get(i));
+        }
+        return perfumeBrand;
+
+    }
+
+
+    public int setMaxListSize(){
+       return perfumeBrand.size();
+    }
 }
