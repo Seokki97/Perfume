212dfbd8b31663d4bf16deb253659be73aac1e3c
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-28 19:29:53 +0900

    feat - 향수 이름,브랜드,특징 조회 메서드 구현

--- src/main/java/com/example/perfume/perfume/repository/PerfumeRepository.java
+++ src/main/java/com/example/perfume/perfume/repository/PerfumeRepository.java
@@ -4,6 +4,13 @@ import com.example.perfume.perfume.domain.Perfume;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PerfumeRepository extends JpaRepository<Perfume, Long> {
+    Optional<Perfume> findById(Long id);
+    Optional<Perfume> findByPerfumeName(String perfumeName);
+    Optional<Perfume> findByPerfumeFeature(String perfumeFeature);
+
+    Optional<Perfume> findByPerfumeBrand(String perfumeBrand);
 }
