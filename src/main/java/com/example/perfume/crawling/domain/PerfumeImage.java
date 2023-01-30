4d0cf44bb566c99430b34ef95b747e81c6ad2ead
JunSeok Kim <skaksdl1238@gmail.com>
2023-01-30 20:45:55 +0900

    feat - crawling Image Url!

---
+++ src/main/java/com/example/perfume/crawling/domain/PerfumeImage.java
@@ -0,0 +1,15 @@
+package com.example.perfume.crawling.domain;
+
+public class PerfumeImage {
+    private String perfumeImageUrl;
+
+
+    public PerfumeImage(String perfumeImageUrl){
+        this.perfumeImageUrl = perfumeImageUrl;
+    }
+    @Override
+    public String toString(){
+        return perfumeImageUrl;
+    }
+
+}
