6e5af10a5f21adc7cc75f017810b398cc6fb2444
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-02 00:41:53 +0900

    feat - Create PerfumeBrand.java

---
+++ src/main/java/com/example/perfume/crawling/domain/PerfumeBrand.java
@@ -0,0 +1,14 @@
+package com.example.perfume.crawling.domain;
+
+public class PerfumeBrand {
+    private String perfumeBrand;
+
+    public PerfumeBrand(String perfumeBrand){
+        this.perfumeBrand = perfumeBrand;
+    }
+
+    @Override
+    public String toString(){
+        return perfumeBrand;
+    }
+}
