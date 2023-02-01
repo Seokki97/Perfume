cbe8b85d7c948bcb28f4b8669e5d0338f7312534
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-02 01:01:48 +0900

    refactor - Constructor Annotation 제거, 코드 간결화

--- src/main/java/com/example/perfume/perfume/service/PerfumeService.java
+++ src/main/java/com/example/perfume/perfume/service/PerfumeService.java
@@ -5,7 +5,6 @@ import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeDto;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeRequestDto;
 import com.example.perfume.perfume.repository.PerfumeRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -13,17 +12,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class PerfumeService {
     private final PerfumeRepository perfumeRepository;
 
     private final CSVFileLoading csvFileLoading;
 
-    public void savePerfumeData(Long id, PerfumeDto perfumeDto) throws IOException {
+    public PerfumeService(PerfumeRepository perfumeRepository, CSVFileLoading csvFileLoading){
+        this.perfumeRepository = perfumeRepository;
+        this.csvFileLoading = csvFileLoading;
+    }
+    public void savePerfumeData(Long id) throws IOException {
         csvFileLoading.extractAllPerfumeData();
         for (int i = 0; i < csvFileLoading.setMaxListSize(); i++) {
 
-            perfumeDto = new PerfumeDto(id,
+            PerfumeDto perfumeDto = new PerfumeDto(id,
                     csvFileLoading.getPerfumeName().get(i),
                     csvFileLoading.getPerfumeFeature().get(i),
                     csvFileLoading.getPerfumeBrand().get(i),
@@ -57,10 +59,7 @@ public class PerfumeService {
     }
 
     public boolean isExistPerfumeName(PerfumeRequestDto perfumeRequestDto) {
-        if (perfumeRepository.existsByPerfumeName(perfumeRequestDto.getPerfumeName())) {
-            return true;
-        }
-        return false;
+        return perfumeRepository.existsByPerfumeName(perfumeRequestDto.getPerfumeName());
     }
 
 }
