1fa9d083caee8de96e4a250ebbcc12b84f416aa4
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-06 15:46:55 +0900

    feat - SurveyCSVFileLoading 구현

---
+++ src/main/java/com/example/perfume/crawling/service/SurveyCSVFileLoading.java
@@ -0,0 +1,67 @@
+package com.example.perfume.crawling.service;
+
+import com.example.perfume.crawling.domain.perfume.PerfumeList;
+import com.example.perfume.crawling.domain.survey.SurveyList;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SurveyCSVFileLoading extends CSVFileLoading {
+
+    private List<String> surveyListTest;
+
+    private static final int COLUMN_LENGTH = 5;
+
+    private static final int FIRST_COLUMN = 0;
+    private static final int SECOND_COLUMN = 1;
+    private static final int THIRD_COLUMN = 2;
+    private static final int FOURTH_COLUMN = 3;
+    private static final int FIFTH_COLUMN = 4;
+
+    public SurveyCSVFileLoading(List<String> surveyListTest) throws FileNotFoundException, UnsupportedEncodingException {
+
+        this.surveyListTest = surveyListTest;
+    }
+
+    public List<String> makeSurveyList(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            surveyListTest.add(array[i]);
+
+        }
+        return surveyListTest;
+    }
+
+    public List<String> splitSurveyData() throws IOException {
+        String surveyData;
+        while ((surveyData = bufferedReader.readLine()) != null) {
+            String[] line = surveyData.split(",");
+            makeSurveyList(line);
+        }
+        return surveyListTest;
+    }
+
+    public List<String> extractAnswerFromSurvey(int columnName) throws IOException {
+        splitSurveyData();
+        List<String> testList = new ArrayList<>();
+        for (int i = columnName; i < surveyListTest.size(); i += COLUMN_LENGTH) {
+            testList.add(surveyListTest.get(i));
+        }
+
+        return testList;
+    }
+
+    public SurveyList extractAllSurveyData(SurveyList surveyList) throws IOException {
+        surveyList = surveyList.builder().firstAnswer(extractAnswerFromSurvey(FIRST_COLUMN))
+                .secondAnswer(extractAnswerFromSurvey(SECOND_COLUMN))
+                .thirdAnswer(extractAnswerFromSurvey(THIRD_COLUMN))
+                .fourthAnswer(extractAnswerFromSurvey(FOURTH_COLUMN))
+                .fifthAnswer(extractAnswerFromSurvey(FIFTH_COLUMN))
+                .build();
+        return surveyList;
+    }
+}
