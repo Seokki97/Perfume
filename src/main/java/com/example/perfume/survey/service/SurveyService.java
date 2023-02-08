f599b2a7eb413a2d9307de98fe9e1a607fff0af6
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-08 19:02:08 +0900

    feat - 예외처리, 빈 데이터 발생 시 대응

--- src/main/java/com/example/perfume/survey/service/SurveyService.java
+++ src/main/java/com/example/perfume/survey/service/SurveyService.java
@@ -1,5 +1,6 @@
 package com.example.perfume.survey.service;
 
+import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.survey.domain.Survey;
 import com.example.perfume.survey.dto.featureDto.SurveyResponseDto;
 import com.example.perfume.survey.repository.SurveyRepository;
@@ -26,7 +27,7 @@ public class SurveyService {
                 .collect(Collectors.toList());
     }
 
-    private List<Survey> addList(List<Survey> firstDataList, List<Survey> secondDataList) {
+    public List<Survey> addList(List<Survey> firstDataList, List<Survey> secondDataList) {
         List<Survey> addedList = new ArrayList<>();
         addedList.addAll(firstDataList);
         addedList.addAll(secondDataList);
@@ -59,19 +60,44 @@ public class SurveyService {
                 surveyResponseDto.getFifthAnswerOfSurvey()), surveyRepository.findByFifthAnswerOfSurvey("디폴트"));
     }
 
+    private List<Survey> isEmptyFinalResult(List<Survey> finalResult, List<Survey> beforeResult) {
+        if (finalResult.isEmpty()) {
+            return beforeResult;
+        }
+        return finalResult;
+    }
+
+    private List<Survey> isEmptyMoodColumn(SurveyResponseDto surveyResponseDto, List<Survey> secondAnswer) {
+        if (secondAnswer.isEmpty()) {
+            //비었으면 그 해당 데이터를 제외한 나머지 데이터를 다시 리스트로 가져와야함.
+            return retrySecondFiltering(surveyResponseDto);
+        }
+        return secondAnswer;
+    }
+
+    private List<Survey> retrySecondFiltering(SurveyResponseDto surveyResponseDto) {
+        return compareTwoFilteredSurveyData(compareTwoFilteredSurveyData(addFirstAnswerList(surveyResponseDto)
+                        , filterSecondAnswer(surveyResponseDto))
+                , surveyRepository.findByThirdAnswerOfSurveyNotContaining(surveyResponseDto.getThirdAnswerOfSurvey()));
+    }
+
     public List<Survey> findDataFromAnswerTest(SurveyResponseDto surveyResponseDto) {
 
-        List<Survey> firstFilteringList = compareTwoFilteredSurveyData(addFirstAnswerList(surveyResponseDto), filterSecondAnswer(surveyResponseDto)); //첫번째 두번째 같은거 찾기
+        List<Survey> firstFilteringList = compareTwoFilteredSurveyData(addFirstAnswerList(surveyResponseDto), filterSecondAnswer(surveyResponseDto));
 
         List<Survey> secondFilteringList = compareTwoFilteredSurveyData(firstFilteringList, filterThirdAnswer(surveyResponseDto));
 
-        List<Survey> thirdFilteringList = compareTwoFilteredSurveyData(secondFilteringList, addFourthAnswerList(surveyResponseDto));
+        List<Survey> thirdFilteringList = compareTwoFilteredSurveyData(isEmptyMoodColumn(surveyResponseDto, secondFilteringList), addFourthAnswerList(surveyResponseDto));
 
         List<Survey> filteringListResult = compareTwoFilteredSurveyData(thirdFilteringList, addFifthAnswerList(surveyResponseDto));
+        isEmptyFinalResult(filteringListResult, thirdFilteringList);
 
         return filteringListResult;
     }
 
+    public List<Survey> extractPerfumeDataFromSurvey(SurveyResponseDto surveyResponseDto) {
+        return findDataFromAnswerTest(surveyResponseDto);
+    }
 
-
+    //이제 최종 결과물에 대한 perfume 객체를 리턴해주면 됨
 }
