de4394733e9346e0dc59ff8101978a290b691c0b
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-12 16:05:12 +0900

    feat - 메세지 처리 테스트코드 작성

---
+++ src/test/java/com/example/perfume/survey/service/FeatureServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.perfume.survey.service;
+
+
+import com.example.perfume.perfume.repository.PerfumeRepository;
+import com.example.perfume.survey.domain.Survey;
+import com.example.perfume.survey.message.ScentMessage;
+import com.example.perfume.survey.message.SeasonMessage;
+import com.example.perfume.survey.repository.SurveyRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FeatureServiceTest {
+
+    @Autowired
+    private PerfumeRepository perfumeRepository;
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    @Autowired
+    private FeatureService featureService;
+
+    @DisplayName("선택된 향에 맞는 메세지가 출력된다.")
+    @Test
+    void selectScent() {
+        long perfumeId = 1l;
+
+        String actual = featureService.selectScent(perfumeId);
+
+        String expected = ScentMessage.FLORAL_MESSAGE;
+
+        assertAll(
+                () -> assertEquals(actual, expected)
+        );
+    }
+
+    @DisplayName("선택된 계절에 맞는 메세지가 출력된다.")
+    @Test
+    void selectSeason() {
+        long perfumeId = 1l;
+
+        String actual = featureService.selectSeason(perfumeId);
+
+        String expected = "겨울" + SeasonMessage.SEASON_MESSAGE;
+
+        assertAll(
+                () -> assertEquals(actual, expected)
+        );
+    }
+}
