66446edb740e6f4713ae32823f1da6b27f0a8ebc
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-16 12:39:26 +0900

    feat - 배포시 리액트 port와의 cors문제 해결을 위한 Config

---
+++ src/main/java/com/example/perfume/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.perfume.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+    }
+}
