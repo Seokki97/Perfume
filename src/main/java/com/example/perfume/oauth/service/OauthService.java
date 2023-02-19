5a3a98080d267e2c11cebfc81ea23f1ff74c8ba9
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-19 12:02:01 +0900

    feat - 토큰 타입을 받아와서 respose 내려주기

--- src/main/java/com/example/perfume/oauth/service/OauthService.java
+++ src/main/java/com/example/perfume/oauth/service/OauthService.java
@@ -1,9 +1,20 @@
 package com.example.perfume.oauth.service;
 
 import com.example.perfume.oauth.OauthType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Service
 public class OauthService {
@@ -19,14 +30,28 @@ public class OauthService {
 
     public LinkedMultiValueMap<String, String> setHttpBody(String code) {
 
-        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add(OauthType.GRANT_TYPE.getName(), OauthType.GRANT_TYPE.getType());
-        params.add(OauthType.CLIENT_ID.getName(), OauthType.CLIENT_ID.getType());
-        params.add(OauthType.REDIRECT_URI.getName(), OauthType.REDIRECT_URI.getType());
-        params.add("code", code);
-        return params;
+        LinkedMultiValueMap<String, String> accessTokenParams = new LinkedMultiValueMap<>();
+        accessTokenParams.add(OauthType.GRANT_TYPE.getName(), OauthType.GRANT_TYPE.getType());
+        accessTokenParams.add(OauthType.CLIENT_ID.getName(), OauthType.CLIENT_ID.getType());
+        accessTokenParams.add(OauthType.REDIRECT_URI.getName(), OauthType.REDIRECT_URI.getType());
+        accessTokenParams.add("code", code);
+        return accessTokenParams;
     }
 
+    public RestTemplate getRestTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+
+    public ResponseEntity<String> getResponse(String code){
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>((setHttpBody(code)),setHttpHeaders());
+
+        String url = "https://kauth.kakao.com/oauth/token";
+
+        ResponseEntity<String> response = getRestTemplate().postForEntity(url, request, String.class);
+
+        return response;
+    }
 
 
 
