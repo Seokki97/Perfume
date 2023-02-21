1a552992575538acfd40397c0c851df0e1690d85
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-21 13:00:19 +0900

    feat - 스프링 시큐리티 Configuration 생성

---
+++ src/main/java/com/example/perfume/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.perfume.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@NoArgsConstructor
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/perfume/*","/post/*","/member/*","/oauth/*","/survey/*").permitAll()
+                .and()
+                .addFilterBefore(null, UsernamePasswordAuthenticationFilter.class);
+    }
+}
