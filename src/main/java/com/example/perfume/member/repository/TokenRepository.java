6feb4c8fca12cf6d30c247bce756fae23711156e
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-23 13:00:33 +0900

    feat - 만료된 토큰을 삭제

--- src/main/java/com/example/perfume/member/repository/TokenRepository.java
+++ src/main/java/com/example/perfume/member/repository/TokenRepository.java
@@ -3,6 +3,10 @@ package com.example.perfume.member.repository;
 import com.example.perfume.member.domain.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
+    Optional<Token> findByMemberId(Long memberId);
+    Optional<Token> deleteByMemberId(Long memberId);
 }
