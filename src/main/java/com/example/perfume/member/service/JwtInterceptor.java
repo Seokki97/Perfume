5fb2e6ca7c6f3d8d517b77f1e6cf3de4bac3ef1a
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-26 13:59:46 +0900

    feat - jwt인터셉터 구현

--- src/main/java/com/example/perfume/member/service/JwtInterceptor.java
+++ src/main/java/com/example/perfume/member/service/JwtInterceptor.java
@@ -1,11 +1,16 @@
 package com.example.perfume.member.service;
 
 
+import com.example.perfume.advice.BadRequestException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
@@ -18,8 +23,17 @@ public class JwtInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler){
-
-        return true;
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) throws IOException {
+        String refreshToken = httpServletRequest.getHeader("X-REFRESH-TOKEN");
+        try {
+            if (jwtProvider.validateToken(refreshToken)) {
+                return true;
+            }
+            throw new BadRequestException("권한이 없는 토큰입니다");
+        } catch (MalformedJwtException e) {
+            throw new BadRequestException("위조된 토큰입니다.");
+        } catch (ExpiredJwtException e) {
+            throw new BadRequestException("만료된 토큰입니다.");
+        }
     }
 }
