eefa21bdc8ca855b5e665e94906c465a5a3be18f
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-27 15:44:19 +0900

    feat - 타인의 향수추천 기능 (조회) api 생성

--- src/main/java/com/example/perfume/member/repository/RecommendRepository.java
+++ src/main/java/com/example/perfume/member/repository/RecommendRepository.java
@@ -1,9 +1,15 @@
 package com.example.perfume.member.repository;
 
-import com.example.perfume.member.domain.Recommend;
+import com.example.perfume.member.domain.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+public interface RecommendRepository extends JpaRepository<Recommendation, Long> {
+
+    List<Recommendation> findByMemberId(Long memberId);
+
 }
