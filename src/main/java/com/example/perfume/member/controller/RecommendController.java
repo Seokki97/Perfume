5057f08cc393cd73e73e2b5ab19d0aad7fe2968f
JunSeok Kim <skaksdl1238@gmail.com>
2023-02-27 15:43:44 +0900

    feat - 타인의 향수추천 기능 (조회) api 생성

--- src/main/java/com/example/perfume/member/controller/RecommendController.java
+++ src/main/java/com/example/perfume/member/controller/RecommendController.java
@@ -1,23 +1,30 @@
 package com.example.perfume.member.controller;
 
-import com.example.perfume.member.dto.memberDto.MemberRequestDto;
-import com.example.perfume.member.service.RecommendService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.perfume.member.domain.Recommendation;
+import com.example.perfume.member.dto.recommendDto.RecommendRequestDto;
+import com.example.perfume.member.service.RecommendationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/member")
 public class RecommendController {
-    private final RecommendService recommendService;
+    private final RecommendationService recommendationService;
 
-    public RecommendController(RecommendService recommendService) {
-        this.recommendService = recommendService;
+    public RecommendController(RecommendationService recommendationService) {
+        this.recommendationService = recommendationService;
     }
 
     @PostMapping("/recommend/{id}")
-    public void recommendPerfume(@PathVariable("id") MemberRequestDto memberRequestDto) {
+    public void recommendPerfume(@PathVariable("id") Long id, @RequestBody RecommendRequestDto recommendRequestDto) {
+        recommendationService.recommendByOtherGuest(id, recommendRequestDto);
 
+    }
+
+    @GetMapping("/show-recommended-perfume")
+    public ResponseEntity<List<Recommendation>> showRecommendedPerfume() {
+        return ResponseEntity.ok(recommendationService.showRecommendedPerfume());
     }
 }
