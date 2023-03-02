edb197681a1c472b3164785cf1d4fdd97d8ab62c
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-02 20:18:37 +0900

    feat - Mood Type 추가

---
+++ src/main/java/com/example/perfume/survey/domain/MoodType.java
@@ -0,0 +1,27 @@
+package com.example.perfume.survey.domain;
+
+public enum MoodType {
+    //따뜻한
+    WARMTH(),
+    //산뜻한
+    NEAT(),
+    //관능적인
+    SENSUAL(),
+    //시크한
+    CHIC(),
+    //차분한
+    TRANQUIL(),
+    //깨끗한
+    CLEAN(),
+    //포근한
+    COZY(),
+    //세련된
+    REFINED(),
+    //상큼한
+    FRESH(),
+    //달콤한
+    SWEET(),
+    //싱그러운
+    REFRESHING(),
+
+}
