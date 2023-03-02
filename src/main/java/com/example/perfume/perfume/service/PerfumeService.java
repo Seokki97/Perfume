e90eaf4e709ef8e1c772ee94fc5c8adc6b66347f
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-02 14:22:48 +0900

    refactor - PerfumeNotFoundException처리

--- src/main/java/com/example/perfume/perfume/service/PerfumeService.java
+++ src/main/java/com/example/perfume/perfume/service/PerfumeService.java
@@ -5,18 +5,13 @@ import com.example.perfume.crawling.service.PerfumeCSVFileLoading;
 import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeRequestDto;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeResponseDto;
-import com.example.perfume.perfume.exception.BrandNotFoundException;
 import com.example.perfume.perfume.exception.PerfumeNotFoundException;
 import com.example.perfume.perfume.repository.PerfumeRepository;
-import com.example.perfume.survey.dto.featureDto.FeatureResponseDto;
-import com.example.perfume.survey.service.FeatureService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 @Service
 public class PerfumeService {
@@ -38,11 +33,10 @@ public class PerfumeService {
         return perfumeResponseDto;
     }
 
-    public void savePerfumeData(Long id, PerfumeList perfumeList) throws IOException {
-        perfumeList = perfumeCsvFileLoading.extractAllPerfumeData(perfumeList);
-
-        for (int firstIndex = 0; firstIndex < perfumeList.getMaxSize(); firstIndex++) {
-            Perfume perfumeDataSet = makePerfumeList(id, firstIndex, perfumeList).toEntity();
+    public void savePerfumeData(Long id, PerfumeList perfume) throws IOException {
+        perfume = perfumeCsvFileLoading.extractAllPerfumeData(perfume);
+        for (int firstIndex = 0; firstIndex < perfume.getMaxSize(); firstIndex++) {
+            Perfume perfumeDataSet = makePerfumeList(id, firstIndex, perfume).toEntity();
             perfumeRepository.save(perfumeDataSet);
         }
     }
@@ -50,13 +44,13 @@ public class PerfumeService {
     public List<Perfume> findPerfumeByName(PerfumeRequestDto perfumeRequestDto) {
 
         List<Perfume> perfume = perfumeRepository.findByPerfumeNameContaining(perfumeRequestDto.getPerfumeName());
-
+        isPerfumeListEmpty(perfume);
         return perfume;
     }
 
     public List<Perfume> findPerfumeByBrand(PerfumeRequestDto perfumeRequestDto) {
         List<Perfume> perfume = perfumeRepository.findByBrandNameContaining(perfumeRequestDto.getBrandName());
-
+        isPerfumeListEmpty(perfume);
         return perfume;
     }
 
@@ -68,4 +62,10 @@ public class PerfumeService {
         return perfumeRepository.findAll().stream().collect(Collectors.toList());
     }
 
+    public boolean isPerfumeListEmpty(List<Perfume> perfumeList) {
+        if (perfumeList.isEmpty()) {
+            throw new PerfumeNotFoundException();
+        }
+        return true;
+    }
 }
