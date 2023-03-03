230dea0a789d9db40e40fcff3fe94107d1c786a6
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-03 19:29:11 +0900

    test - 메세지처리 테스트코드 작성

--- src/test/java/com/example/perfume/survey/service/FeatureServiceTest.java
+++ src/test/java/com/example/perfume/survey/service/FeatureServiceTest.java
@@ -1,24 +1,26 @@
 package com.example.perfume.survey.service;
 
 
+import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.repository.PerfumeRepository;
+import com.example.perfume.perfume.service.PerfumeService;
+import com.example.perfume.survey.domain.MoodType;
 import com.example.perfume.survey.domain.ScentType;
 import com.example.perfume.survey.domain.SeasonType;
+import com.example.perfume.survey.domain.Survey;
 import com.example.perfume.survey.repository.SurveyRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class FeatureServiceTest {
-
-    @Autowired
-    private PerfumeRepository perfumeRepository;
     @Autowired
     private SurveyRepository surveyRepository;
 
@@ -28,28 +30,60 @@ public class FeatureServiceTest {
     @DisplayName("선택된 향에 맞는 메세지가 출력된다.")
     @Test
     void selectScent(){
-        long perfumeId = 1l;
-
-        String actual = featureService.selectScent(perfumeId);
+        Survey survey = Survey.builder()
+                .id(1l)
+                .genderAnswer("여자")
+                .scentAnswer("플로럴")
+                .moodAnswer("상큼한")
+                .styleAnswer("포멀")
+                .seasonAnswer("여름")
+                .build();
+        surveyRepository.save(survey);
+        String actual = featureService.selectScent(survey.getId());
 
         String expected = ScentType.FLORAL.getFeature();
 
-        assertAll(
-                () -> assertEquals(expected,actual)
-        );
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("선택된 계절에 맞는 메세지가 출력된다.")
     @Test
     void selectSeason(){
-        long perfumeId = 1l;
+        Survey survey = Survey.builder()
+                .id(1l)
+                .genderAnswer("여자")
+                .scentAnswer("플로럴")
+                .moodAnswer("상큼한")
+                .styleAnswer("포멀")
+                .seasonAnswer("여름")
+                .build();
 
-        String actual = featureService.selectSeason(perfumeId);
+        surveyRepository.save(survey);
+        String actual = featureService.selectSeason(survey.getId());
 
-        String expected = SeasonType.WINTER.getFeature();
+        String expected = SeasonType.SUMMER.getFeature();
 
-        assertAll(
-                () -> assertEquals(expected,actual)
-        );
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("선택된 무드에 맞는 메세지가 출력된다.")
+    @Test
+    void selectMood(){
+        Survey survey = Survey.builder()
+                .id(1l)
+                .genderAnswer("여자")
+                .scentAnswer("플로럴")
+                .moodAnswer("상큼한")
+                .styleAnswer("포멀")
+                .seasonAnswer("여름")
+                .build();
+
+        surveyRepository.save(survey);
+        String actual = featureService.selectMood(survey.getId());
+
+        String expected = MoodType.FRESH.getMessage();
+
+        assertThat(actual).isEqualTo(expected);
+
     }
 }
