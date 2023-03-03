56363d58125be7fdd3a83440767d44703e5aae4e
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-03 20:27:06 +0900

    refactor - DataService가 향수와 설문 Db를 참조하지 않게 수정

--- src/main/java/com/example/perfume/survey/service/DataService.java
+++ src/main/java/com/example/perfume/survey/service/DataService.java
@@ -2,51 +2,42 @@ package com.example.perfume.survey.service;
 
 import com.example.perfume.crawling.domain.survey.SurveyList;
 import com.example.perfume.crawling.service.SurveyCSVFileLoading;
-import com.example.perfume.perfume.domain.Perfume;
-import com.example.perfume.perfume.repository.PerfumeRepository;
+import com.example.perfume.perfume.service.PerfumeService;
 import com.example.perfume.survey.domain.Survey;
 import com.example.perfume.survey.dto.surveyDto.SurveyResponseDto;
-import com.example.perfume.survey.repository.SurveyRepository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class DataService {
-
-    private final SurveyRepository surveyRepository;
     private final SurveyCSVFileLoading surveyCSVFileLoading;
-    private final PerfumeRepository perfumeRepository;
+    private final PerfumeService perfumeService;
+    private final SurveyService surveyService;
 
-    public DataService(SurveyRepository surveyRepository, SurveyCSVFileLoading surveyCSVFileLoading, PerfumeRepository perfumeRepository) {
-        this.surveyRepository = surveyRepository;
+    public DataService(SurveyService surveyService, SurveyCSVFileLoading surveyCSVFileLoading, PerfumeService perfumeService) {
+        this.surveyService = surveyService;
         this.surveyCSVFileLoading = surveyCSVFileLoading;
-        this.perfumeRepository = perfumeRepository;
+        this.perfumeService = perfumeService;
 
     }
 
     public SurveyResponseDto makeList(Long id, int firstIndex, SurveyList surveyList) {
-        SurveyResponseDto surveyResponseDto = new SurveyResponseDto(id,
+        return new SurveyResponseDto(id,
                 surveyList.getFirstAnswer().get(firstIndex),
                 surveyList.getSecondAnswer().get(firstIndex),
                 surveyList.getThirdAnswer().get(firstIndex),
                 surveyList.getFourthAnswer().get(firstIndex),
                 surveyList.getFifthAnswer().get(firstIndex));
-        return surveyResponseDto;
     }
 
     public void saveSurveyData(Long id, SurveyList surveyList) throws IOException {
         surveyList = surveyCSVFileLoading.extractAllSurveyData(surveyList);
         for (int firstIndex = 0; firstIndex < surveyList.getMaxSize(); firstIndex++) {
             Long secondIndex = (long) firstIndex + 1;
-            Survey surveyDataSet = makeList(id, firstIndex, surveyList).toEntity(getPerfumeId(secondIndex));
-            surveyRepository.save(surveyDataSet);
+            Survey surveyDataSet = makeList(id, firstIndex, surveyList).toEntity(perfumeService.findPerfumeById(secondIndex));
+            surveyService.saveSurveyData(surveyDataSet);
         }
     }
-
-    public Perfume getPerfumeId(Long id) {
-        return perfumeRepository.findById(id).get();
-    }
-
 
 }
