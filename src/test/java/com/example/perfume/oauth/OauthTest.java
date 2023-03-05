90c1dbf98f2fc6652b13e18787569cbc9bf90396
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-05 15:02:47 +0900

    test - 회원 정보 저장 테스트코드 작성

--- src/test/java/com/example/perfume/oauth/OauthTest.java
+++ src/test/java/com/example/perfume/oauth/OauthTest.java
@@ -1,6 +1,9 @@
 package com.example.perfume.oauth;
 
 
+import com.example.perfume.member.domain.Member;
+import com.example.perfume.member.dto.memberDto.MemberRequestDto;
+import com.example.perfume.member.service.MemberService;
 import com.example.perfume.oauth.exception.EmailNotFoundException;
 import com.example.perfume.oauth.service.OauthService;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +21,9 @@ public class OauthTest {
     @Autowired
     private OauthService oauthService;
 
+    @Autowired
+    private MemberService memberService;
+
     @DisplayName("이메일 사용을 동의하지 않았을 시 예외가 발생한다.")
     @Test
     void isAgreeEmailCheck(){
@@ -25,6 +31,27 @@ public class OauthTest {
 
         assertThatThrownBy(() -> oauthService.isAgreeEmailUsing(email))
                 .isInstanceOf(EmailNotFoundException.class).hasMessage("계정 이용 동의를 하지 않았습니다.");
+    }
+
+    @DisplayName("회원 정보를 DB에 저장시킨다.")
+    @Test
+    void saveMember(){
+        MemberRequestDto memberRequestDto = MemberRequestDto.builder()
+                .id(1l)
+                .memberId(123l)
+                .email("ask@naver.com")
+                .nickname("서키")
+                .build();
+        oauthService.saveUserProfile(memberRequestDto);
+       Member actual =  memberService.findByMemberPk(memberRequestDto.getMemberId());
+        Member expected = Member.builder()
+                .id(memberRequestDto.getId())
+                .email(memberRequestDto.getEmail())
+                .memberId(memberRequestDto.getMemberId())
+                .nickname(memberRequestDto.getNickname())
+                .build();
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
 }
