3461c5831e55d97830b9c0a164c294ba199d07b8
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-05 14:35:04 +0900

    refactor - id 추가

--- src/main/java/com/example/perfume/member/dto/memberDto/MemberRequestDto.java
+++ src/main/java/com/example/perfume/member/dto/memberDto/MemberRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.perfume.member.dto.memberDto;
 
 import com.example.perfume.member.domain.Member;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MemberRequestDto {
+    private Long id;
     private Long memberId;
     private String nickname;
 
@@ -13,7 +15,9 @@ public class MemberRequestDto {
     public MemberRequestDto() {
     }
 
-    public MemberRequestDto(Long memberId ,String nickname, String email) {
+    @Builder
+    public MemberRequestDto(Long id, Long memberId ,String nickname, String email) {
+        this.id = id;
         this.nickname = nickname;
         this.email = email;
         this.memberId = memberId;
