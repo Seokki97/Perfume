cdd159378e0f101a0a1f9d2d59f7d0786e97022f
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-08 13:37:01 +0900

    feat - Oauth 토큰 발행 테스트

--- src/main/java/com/example/perfume/oauth/controller/OauthController.java
+++ src/main/java/com/example/perfume/oauth/controller/OauthController.java
@@ -2,6 +2,7 @@ package com.example.perfume.oauth.controller;
 
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.member.dto.memberDto.LoginResponse;
+import com.example.perfume.member.dto.memberDto.MemberRequestDto;
 import com.example.perfume.member.service.LoginService;
 import com.example.perfume.oauth.service.OauthService;
 import org.json.simple.parser.ParseException;
@@ -17,16 +18,23 @@ public class OauthController {
     private final OauthService oauthService;
 
     private final LoginService loginService;
-    public OauthController(OauthService oauthService,LoginService loginService) {
+
+    public OauthController(OauthService oauthService, LoginService loginService) {
         this.oauthService = oauthService;
         this.loginService = loginService;
     }
-    @GetMapping("/login")
-    public ResponseEntity<LoginResponse> signUp(@RequestParam String code, HttpSession httpSession) throws ParseException {
 
-        Member member = oauthService.loadUserProfile(code,httpSession);
+    @GetMapping("/login")
+    public ResponseEntity<LoginResponse> signUp(@RequestParam String code, HttpSession httpSession) {
+
+        Member member = oauthService.loadUserProfile(code, httpSession);
 
         return ResponseEntity.ok(loginService.generateToken(member.getMemberId()));
     }
 
+    @GetMapping("/test")
+    public ResponseEntity<LoginResponse> save(@RequestBody MemberRequestDto memberRequestDto){
+        oauthService.saveUserProfile(memberRequestDto);
+        return ResponseEntity.ok(loginService.generateToken(memberRequestDto.getMemberId()));
+    }
 }
