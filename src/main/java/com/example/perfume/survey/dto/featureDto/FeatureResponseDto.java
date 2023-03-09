17964565bc1dab1ae4c4b46a5c620fd439804193
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-09 22:48:28 +0900

    feat - 특징 dto builder 생성

--- src/main/java/com/example/perfume/survey/dto/featureDto/FeatureResponseDto.java
+++ src/main/java/com/example/perfume/survey/dto/featureDto/FeatureResponseDto.java
@@ -5,16 +5,23 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class FeatureResponseDto {
 
     private String scentRecommend;
     private String moodRecommend;
     private String seasonRecommend;
-
     private Perfume perfume;
 
     public FeatureResponseDto() {
+
+    }
+
+    @Builder
+    public FeatureResponseDto(String scentRecommend, String moodRecommend, String seasonRecommend, Perfume perfume) {
+        this.scentRecommend = scentRecommend;
+        this.moodRecommend = moodRecommend;
+        this.seasonRecommend = seasonRecommend;
+        this.perfume = perfume;
 
     }
 
