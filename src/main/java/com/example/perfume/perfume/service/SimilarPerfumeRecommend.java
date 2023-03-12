b2622fd1c32bfc64214aa5cffffd9876368dcc2c
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-12 14:10:34 +0900

    fix - 유사향수 추천 버그 수정 ,메서드 정리 필요

--- src/main/java/com/example/perfume/perfume/service/SimilarPerfumeRecommend.java
+++ src/main/java/com/example/perfume/perfume/service/SimilarPerfumeRecommend.java
@@ -1,24 +1,32 @@
 package com.example.perfume.perfume.service;
 
+import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeResponseDto;
+import com.example.perfume.perfume.exception.PerfumeNotFoundException;
+import com.example.perfume.perfume.repository.PerfumeRepository;
 import com.example.perfume.survey.domain.Survey;
 import com.example.perfume.survey.repository.SurveyRepository;
 import com.example.perfume.survey.service.SurveyService;
 import com.example.perfume.survey.service.SurveyUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SimilarPerfumeRecommend {
     private final SurveyRepository surveyRepository;
     private final SurveyUtil surveyUtil;
     private final SurveyService surveyService;
+    private final PerfumeRepository perfumeRepository;
 
-    public SimilarPerfumeRecommend(SurveyRepository surveyRepository, SurveyUtil surveyUtil, SurveyService surveyService) {
+    public SimilarPerfumeRecommend(SurveyRepository surveyRepository, SurveyUtil surveyUtil, SurveyService surveyService,
+                                   PerfumeRepository perfumeRepository) {
         this.surveyService = surveyService;
         this.surveyRepository = surveyRepository;
         this.surveyUtil = surveyUtil;
+        this.perfumeRepository = perfumeRepository;
     }
 
     public List<Survey> extractFirstFeature(PerfumeResponseDto perfumeResponseDto) {
@@ -37,11 +45,16 @@ public class SimilarPerfumeRecommend {
         return surveyUtil.addList(extractFirstFeature(perfumeResponseDto), surveyRepository.findByGenderAnswer("젠더리스"));
     }
 
-    public List<Survey> showSimilarPerfume(PerfumeResponseDto perfumeResponseDto) {
+    public List<Perfume> showSimilarPerfume(PerfumeResponseDto perfumeResponseDto) {
         List<Survey> firstComparedList = surveyUtil.compareTwoFilteredSurveyData
                 (addFirstFeatureAndGenderless(perfumeResponseDto), extractSecondFeature(perfumeResponseDto));
-
-        return surveyUtil.compareTwoFilteredSurveyData(firstComparedList, extractThirdFeature(perfumeResponseDto));
+        List<Survey> result = surveyUtil.compareTwoFilteredSurveyData(firstComparedList, extractThirdFeature(perfumeResponseDto));
+        List<Perfume> perfumeList = new ArrayList<>();
+        for (int i = 0; i < result.size(); i++) {
+            perfumeList.add(perfumeRepository.findById(result.get(i).getId()).orElseThrow(PerfumeNotFoundException::new));
+        }
+        return perfumeList;
+        //return surveyUtil.compareTwoFilteredSurveyData(firstComparedList, extractThirdFeature(perfumeResponseDto));
     }
 
 }
