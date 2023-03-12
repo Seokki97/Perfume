6180c96b5dbac9bda8715f71586fd72fb92a0bcd
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-12 21:42:58 +0900

    fix - Lazy Loading으로 인해 Session이 종료된 후에도 엔티티를 로딩하려 하여 버그 발생하는 문제 수정

--- src/main/java/com/example/perfume/member/repository/RecommendRepository.java
+++ src/main/java/com/example/perfume/member/repository/RecommendRepository.java
@@ -2,6 +2,8 @@ package com.example.perfume.member.repository;
 
 import com.example.perfume.member.domain.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.Optional;
 @Repository
 public interface RecommendRepository extends JpaRepository<Recommendation, Long> {
 
-    List<Recommendation> findByMemberId(Long memberId);
+    @Query("SELECT r FROM recommend r JOIN FETCH r.perfume WHERE r.member.id = :memberId")
+    List<Recommendation> findByMemberId(@Param("memberId") Long memberId);
+
+    Optional<Recommendation> findById(Long memberId);
 
 }
