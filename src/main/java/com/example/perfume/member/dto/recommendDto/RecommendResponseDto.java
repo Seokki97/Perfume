b4489463ae2fe9e2126a6ce9df63c633073898bc
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-12 21:48:54 +0900

    refactor- 추천받은 향수 조회 기능을 위해 responseDto 수정

--- src/main/java/com/example/perfume/member/dto/recommendDto/RecommendResponseDto.java
+++ src/main/java/com/example/perfume/member/dto/recommendDto/RecommendResponseDto.java
@@ -3,6 +3,7 @@ package com.example.perfume.member.dto.recommendDto;
 
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.member.domain.Recommendation;
+import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.survey.domain.Survey;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,25 +11,25 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-@Builder
 public class RecommendResponseDto {
 
     private Long id;
 
-    private Member member;
+    private String recommender;
+
+    private String comment;
+
+    private List<Recommendation> recommendationList;
 
     public RecommendResponseDto() {
     }
 
-    public RecommendResponseDto(Long id, Member member, List<Survey> surveyList) {
+    @Builder
+    public RecommendResponseDto(Long id, String recommender,String comment, List<Recommendation> recommendationList) {
         this.id = id;
-        this.member = member;
+        this.recommender = recommender;
+        this.comment = comment;
+        this.recommendationList = recommendationList;
     }
 
-    public Recommendation toEntity() {
-        return Recommendation.builder()
-                .id(id)
-                .member(member)
-                .build();
-    }
 }
