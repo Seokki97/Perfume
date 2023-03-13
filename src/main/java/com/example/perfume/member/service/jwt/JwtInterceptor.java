4f60941e02215b902c81d8278d2a93bc7e8c05b7
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-13 21:59:13 +0900

    feat - 인터셉터 LoginCheck 적용

--- src/main/java/com/example/perfume/member/service/jwt/JwtInterceptor.java
+++ src/main/java/com/example/perfume/member/service/jwt/JwtInterceptor.java
@@ -2,9 +2,13 @@ package com.example.perfume.member.service.jwt;
 
 
 import com.example.perfume.advice.BadRequestException;
+import com.example.perfume.member.exception.TokenExpiredException;
+import com.example.perfume.member.exception.TokenInvalidException;
+import com.example.perfume.member.exception.UserNotFoundException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,17 +25,19 @@ public class JwtInterceptor implements HandlerInterceptor {
     public JwtInterceptor(JwtProvider jwtProvider) {
         this.jwtProvider = jwtProvider;
     }
+
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) throws IOException {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) {
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+        LoginCheck loginCheck = handlerMethod.getMethodAnnotation(LoginCheck.class);
         String accessToken = httpServletRequest.getHeader("Authorization");
-        try {
-            if (!jwtProvider.validateToken(accessToken)) {
-                return false;
-            }
-        } catch (MalformedJwtException e) {
-            throw new BadRequestException("위조된 토큰입니다.");
-        } catch (ExpiredJwtException e) {
-            throw new BadRequestException("만료된 토큰입니다.");
+
+        if (loginCheck == null) {
+            return true;
+        }
+        if (jwtProvider.validateToken(accessToken) == false) {
+            throw new TokenExpiredException();
         }
         return true;
     }
