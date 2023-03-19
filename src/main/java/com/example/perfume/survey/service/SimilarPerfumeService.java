5ce7ce3f7af425bb5ce0a201ee91367450645fe1
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-19 21:31:49 +0900

    refactor - 상수 정의, 젠더리스향수 더해지게 리팩터링

--- src/main/java/com/example/perfume/survey/service/SimilarPerfumeService.java
+++ src/main/java/com/example/perfume/survey/service/SimilarPerfumeService.java
@@ -16,12 +16,15 @@ import java.util.stream.Collectors;
 @Service
 public class SimilarPerfumeService {
 
+    private static final String BLANK = "\\s";
+    private static final int FIRST_MOOD = 0;
+    private static final int MOOD_COLUMN_SIZE = 1;
     private final SurveyService surveyService;
     private final SurveyRepository surveyRepository;
     private final SurveyUtil surveyUtil;
 
     public SimilarPerfumeService(SurveyService surveyService,
-                                 SurveyRepository surveyRepository,SurveyUtil surveyUtil) {
+                                 SurveyRepository surveyRepository, SurveyUtil surveyUtil) {
         this.surveyService = surveyService;
         this.surveyRepository = surveyRepository;
         this.surveyUtil = surveyUtil;
@@ -36,6 +39,7 @@ public class SimilarPerfumeService {
     public List<Perfume> showSimilarPerfume(PerfumeRequestDto perfumeRequestDto) {
         Survey survey = surveyService.findSurveyById(perfumeRequestDto.getId());
         List<Survey> surveyList = surveyRepository.findByGenderAnswerAndScentAnswer(survey.getGenderAnswer(), survey.getScentAnswer());
+        surveyList = surveyUtil.addList(surveyList, surveyRepository.findByGenderAnswerAndScentAnswer(SurveyType.GENDERLESS.getValue(), survey.getScentAnswer()));
         List<Survey> filteredSurveys = surveyService.filterByMood(splitMoodAnswer(survey), surveyList);
         filteredSurveys = findExceptRequestedPerfume(filteredSurveys, SurveyRequestDto.makeDto(survey));
 
@@ -44,17 +48,17 @@ public class SimilarPerfumeService {
 
 
     public SurveyRequestDto splitMoodAnswer(Survey survey) {
-        String[] moodAnswerArray = survey.getMoodAnswer().split("\\s");
-        if (moodAnswerArray.length == 1) {
+        String[] moodAnswerArray = survey.getMoodAnswer().split(BLANK);
+        if (moodAnswerArray.length == MOOD_COLUMN_SIZE) {
             return SurveyRequestDto.builder().moodAnswer(survey.getMoodAnswer()).build();
         }
-        return SurveyRequestDto.builder().moodAnswer(moodAnswerArray[0]).build();
+        return SurveyRequestDto.builder().moodAnswer(moodAnswerArray[FIRST_MOOD]).build();
     }
 
     //만약 비어있을경우에 mood를 split해서 사용
     public String isEmptyMood(List<Survey> perfumeList, Survey survey) {
         if (perfumeList.isEmpty()) {
-            return survey.getMoodAnswer().split(" ")[0];
+            return survey.getMoodAnswer().split(" ")[FIRST_MOOD];
         }
         return survey.getMoodAnswer();
     }
