79df9405dcd36763196bd266836c1cf5f861ce80
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-19 21:44:58 +0900

    feat - 로그인 인터셉터 추가

--- src/main/java/com/example/perfume/config/WebMvcConfig.java
+++ src/main/java/com/example/perfume/config/WebMvcConfig.java
@@ -1,15 +1,29 @@
 package com.example.perfume.config;
 
+import com.example.perfume.member.service.jwt.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+
+    public WebMvcConfig(JwtInterceptor jwtInterceptor) {
+        this.jwtInterceptor = jwtInterceptor;
+    }
+
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry){
+    public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).excludePathPatterns("/member/logout/**");
     }
 }
