9d01aeac0b7bc8f3b7e347f5f4d17d1467bc75ad
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-20 17:41:26 +0900

    refactor -Login Response memberId 추가

--- src/main/java/com/example/perfume/member/dto/loginDto/LoginResponse.java
+++ src/main/java/com/example/perfume/member/dto/loginDto/LoginResponse.java
@@ -11,15 +11,19 @@ public class LoginResponse {
     private String email;
     private String thumbnailImage;
 
+    private Long memberId;
+
     private String accessToken;
     private String refreshToken;
+
     @Builder
-    public LoginResponse(Long id, String nickname, String email, String accessToken, String refreshToken, String thumbnailImage) {
+    public LoginResponse(Long id, String nickname, String email, String accessToken, String refreshToken, String thumbnailImage, Long memberId) {
         this.id = id;
         this.nickname = nickname;
         this.email = email;
         this.thumbnailImage = thumbnailImage;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.memberId = memberId;
     }
 }
