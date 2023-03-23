af483d5340714b1cd70f6db0b28023936ad562de
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-23 14:04:19 +0900

    refactor - GET -> POST로 수정

--- src/main/java/com/example/perfume/feedback/controller/FeedbackController.java
+++ src/main/java/com/example/perfume/feedback/controller/FeedbackController.java
@@ -2,10 +2,7 @@ package com.example.perfume.feedback.controller;
 
 import com.example.perfume.feedback.dto.FeedbackRequestDto;
 import com.example.perfume.feedback.service.FeedbackService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/feedback")
@@ -15,7 +12,7 @@ public class FeedbackController {
     public FeedbackController(FeedbackService feedbackService){
         this.feedbackService = feedbackService;
     }
-    @GetMapping("/save")
+    @PostMapping("/save")
     public void saveFeedback(@RequestBody FeedbackRequestDto feedbackRequestDto){
         feedbackService.saveFeedback(feedbackRequestDto);
     }
