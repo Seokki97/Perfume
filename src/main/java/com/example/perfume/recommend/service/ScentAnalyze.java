e5badf6df50b5351748af65d3de6931882b7521e
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-29 15:09:11 +0900

    refactor - 메서드 순서 변경

--- src/main/java/com/example/perfume/recommend/service/ScentAnalyze.java
+++ src/main/java/com/example/perfume/recommend/service/ScentAnalyze.java
@@ -1,6 +1,7 @@
 package com.example.perfume.recommend.service;
 
 import com.example.perfume.recommend.domain.Recommendation;
+import com.example.perfume.recommend.dto.AnalyzeResponse;
 import com.example.perfume.recommend.dto.ScentAnalyzeResponse;
 import com.example.perfume.recommend.repository.RecommendRepository;
 import org.springframework.stereotype.Service;
@@ -19,26 +20,6 @@ public class ScentAnalyze implements Analyze {
         this.analyzeUtil = analyzeUtil;
     }
 
-
-    public ScentAnalyzeResponse filterMostRecommendedScent(Long memberId) {
-        List<String> scentList = extractRecommendedElement(memberId);
-        Long maxCount = 0L;
-        String scentAnswer = "";
-        int scentListSize = scentList.size();
-        for (int i = 0; i < scentListSize; i++) {
-            Long count = analyzeUtil.countElement(scentList, i);
-            if (count > maxCount) {
-                scentAnswer = scentList.get(i);
-                maxCount = count;
-            }
-        }
-        AnalyzeUtil.isCountingNumberExist(maxCount);
-        return ScentAnalyzeResponse.builder()
-                .scent(scentAnswer)
-                .count(maxCount)
-                .build();
-    }
-
     @Override
     public List<String> extractRecommendedElement(Long memberId) {
         List<Recommendation> recommendationList = recommendRepository.findByMemberId(memberId);
@@ -47,5 +28,16 @@ public class ScentAnalyze implements Analyze {
             perfumeList.add(recommendation.getScentAnswer());
         }
         return perfumeList;
+    }
+
+    public ScentAnalyzeResponse filterMostRecommendedScent(Long memberId) {
+        List<String> scentList = extractRecommendedElement(memberId);
+
+        AnalyzeResponse analyzeResponse = analyzeUtil.countPerfumeList(scentList);
+
+        return ScentAnalyzeResponse.builder()
+                .scent(analyzeResponse.getElementName())
+                .count(analyzeResponse.getCount())
+                .build();
     }
 }
