b729a97a9ed7c3749e2821db4ca47b907277012b
JunSeok Kim <skaksdl1238@gmail.com>
2023-03-31 16:52:05 +0900

    refactor - 기능 개선으로 인해 필요없는 util메서드 삭제

--- src/main/java/com/example/perfume/survey/service/SurveyUtil.java
+++ src/main/java/com/example/perfume/survey/service/SurveyUtil.java
@@ -4,38 +4,18 @@ import com.example.perfume.survey.domain.Survey;
 import com.example.perfume.survey.dto.surveyDto.SurveyRequestDto;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
+
 
 @Service
 public class SurveyUtil {
 
     private static final String BLANK = "\\s";
-    private static final int FIRST_MOOD = 0;
+
     private static final int MOOD_COLUMN_SIZE = 1;
 
-    public List<Survey> compareTwoFilteredSurveyData(List<Survey> firstDataList, List<Survey> secondDataList) {
-        return firstDataList.stream().filter(o -> secondDataList.stream()
-                .anyMatch(n -> o.getId().equals(n.getId()))).collect(Collectors.toList());
-    }
 
-    public List<Survey> addList(List<Survey> firstDataList, List<Survey> secondDataList) {
-        List<Survey> addedList = new ArrayList<>();
-        addedList.addAll(firstDataList);
-        addedList.addAll(secondDataList);
-        return addedList;
-    }
-
-    public List<Survey> isEmptyFinalResult(List<Survey> finalResult, List<Survey> beforeResult) {
-        if (finalResult.isEmpty()) {
-            return beforeResult;
-        }
-        return finalResult;
-    }
-
-    public int getRandomMoodAnswer(String[] moodAnswerArray){
+    public int getRandomMoodAnswer(String[] moodAnswerArray) {
         Random random = new Random();
         return random.nextInt(moodAnswerArray.length);
     }
