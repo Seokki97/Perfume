545c36c10c8db6c36ea72f77dfa0e1cc818bf074
JunSeok Kim <skaksdl1238@gmail.com>
2023-04-11 14:14:13 +0900

    style - GlobalExceptionHandler 개행

--- src/main/java/com/example/perfume/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/perfume/exception/GlobalExceptionHandler.java
@@ -23,72 +23,74 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     private static final int NOT_FOUND_ERROR = 404;
     private static final int UNAUTHORIZED_ERROR = 401;
+
     @ExceptionHandler(ScentNotFoundException.class)
-    public ResponseEntity<?> handleScentNotFoundException(ScentNotFoundException e){
+    public ResponseEntity<?> handleScentNotFoundException(ScentNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(SeasonNotFoundException.class)
-    public ResponseEntity<?> handleSeasonNotFoundException(SeasonNotFoundException e){
+    public ResponseEntity<?> handleSeasonNotFoundException(SeasonNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(MoodNotFoundException.class)
-    public ResponseEntity<?> handleMoodNotFoundException(MoodNotFoundException e){
+    public ResponseEntity<?> handleMoodNotFoundException(MoodNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(SurveyNotFoundException.class)
-    public ResponseEntity<?> handleSurveyNotFoundException(SurveyNotFoundException e){
+    public ResponseEntity<?> handleSurveyNotFoundException(SurveyNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(PostNotFoundException.class)
-    public ResponseEntity<?> handlePostNotFoundException(PostNotFoundException e){
+    public ResponseEntity<?> handlePostNotFoundException(PostNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(PerfumeNotFoundException.class)
-    public ResponseEntity<?> handlePerfumeNotFoundException(PerfumeNotFoundException e){
+    public ResponseEntity<?> handlePerfumeNotFoundException(PerfumeNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(BrandNotFoundException.class)
-    public ResponseEntity<?> handleBrandNotFoundException(BrandNotFoundException e){
+    public ResponseEntity<?> handleBrandNotFoundException(BrandNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(MemberAlreadyExistException.class)
-    public ResponseEntity<?> handleMemberAlreadyExistException(MemberAlreadyExistException e){
+    public ResponseEntity<?> handleMemberAlreadyExistException(MemberAlreadyExistException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(EmailNotFoundException.class)
-    public ResponseEntity<?> handleEmailNotFoundException(EmailNotFoundException e){
+    public ResponseEntity<?> handleEmailNotFoundException(EmailNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleTokenInvalidException(TokenInvalidException e){
+    public ResponseEntity<?> handleTokenInvalidException(TokenInvalidException e) {
         return ResponseEntity.status(UNAUTHORIZED_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException e){
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleRecommendNotFoundException(RecommendNotFoundException e){
+    public ResponseEntity<?> handleRecommendNotFoundException(RecommendNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleTokenExpiredException(TokenExpiredException e){
+    public ResponseEntity<?> handleTokenExpiredException(TokenExpiredException e) {
         return ResponseEntity.status(UNAUTHORIZED_ERROR).body(e.getMessage());
     }
+
     @ExceptionHandler
-    public ResponseEntity<?> handleMemberAlreadyLogoutException(MemberAlreadyLogoutException e){
+    public ResponseEntity<?> handleMemberAlreadyLogoutException(MemberAlreadyLogoutException e) {
         return ResponseEntity.status(UNAUTHORIZED_ERROR).body(e.getMessage());
     }
 }
