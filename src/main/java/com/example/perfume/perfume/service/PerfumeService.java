d18abc2b40f52d8a201b96fd537f5cd2d4cd0272
JunSeok Kim <skaksdl1238@gmail.com>
2023-04-11 14:06:39 +0900

    refactor - boolean -> void타입 변환, 메서드명 isPerfumeListEmpty -> validatePerfumeListEmpty

--- src/main/java/com/example/perfume/perfume/service/PerfumeService.java
+++ src/main/java/com/example/perfume/perfume/service/PerfumeService.java
@@ -1,7 +1,5 @@
 package com.example.perfume.perfume.service;
 
-import com.example.perfume.crawling.domain.perfume.PerfumeList;
-import com.example.perfume.crawling.service.PerfumeCSVFileLoading;
 import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeRequestDto;
 import com.example.perfume.perfume.dto.perfumeDto.PerfumeResponseDto;
@@ -10,7 +8,6 @@ import com.example.perfume.perfume.exception.PerfumeNotFoundException;
 import com.example.perfume.perfume.repository.PerfumeRepository;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,13 +21,13 @@ public class PerfumeService {
 
     public List<Perfume> findPerfumeByName(PerfumeRequestDto perfumeRequestDto) {
         List<Perfume> perfume = perfumeRepository.findByPerfumeNameContaining(perfumeRequestDto.getPerfumeName());
-        isPerfumeListEmpty(perfume);
+        validatePerfumeListEmpty(perfume);
         return perfume;
     }
 
     public List<Perfume> findPerfumeByBrand(PerfumeRequestDto perfumeRequestDto) {
         List<Perfume> perfume = perfumeRepository.findByBrandNameContaining(perfumeRequestDto.getBrandName());
-        isBrandListEmpty(perfume);
+        validateBrandListEmpty(perfume);
         return perfume;
     }
 
@@ -41,22 +38,20 @@ public class PerfumeService {
     public List<Perfume> showAllPerfumeData() {
         List<Perfume> perfumeList = perfumeRepository.findAll();
 
-        isPerfumeListEmpty(perfumeList);
+        validatePerfumeListEmpty(perfumeList);
         return perfumeList;
     }
 
-    private boolean isPerfumeListEmpty(List<Perfume> perfumeList) {
+    private void validatePerfumeListEmpty(List<Perfume> perfumeList) {
         if (perfumeList.isEmpty()) {
             throw new PerfumeNotFoundException();
         }
-        return true;
     }
 
-    private boolean isBrandListEmpty(List<Perfume> brandList) {
+    private void validateBrandListEmpty(List<Perfume> brandList) {
         if (brandList.isEmpty()) {
             throw new BrandNotFoundException();
         }
-        return true;
     }
 
     public List<String> showBrandList() {
