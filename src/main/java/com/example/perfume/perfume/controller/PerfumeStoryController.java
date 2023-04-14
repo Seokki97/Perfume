fd956f5569372e86f65e9562dea64d3405a788b5
JunSeok Kim <skaksdl1238@gmail.com>
2023-04-14 13:36:48 +0900

    docs - PerfumeStoryController 생성

---
+++ src/main/java/com/example/perfume/perfume/controller/PerfumeStoryController.java
@@ -0,0 +1,10 @@
+package com.example.perfume.perfume.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/perfume")
+public class PerfumeStoryController {
+
+}
