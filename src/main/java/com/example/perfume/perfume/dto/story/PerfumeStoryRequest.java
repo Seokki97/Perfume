5ed9f992cff75e8d0fdeaeaad8156696e4c61569
JunSeok Kim <skaksdl1238@gmail.com>
2023-04-14 19:04:16 +0900

    feat - PerfumeStoryrequestDto 디폴트생성자,질문형식 추가

--- src/main/java/com/example/perfume/perfume/dto/story/PerfumeStoryRequest.java
+++ src/main/java/com/example/perfume/perfume/dto/story/PerfumeStoryRequest.java
@@ -17,6 +17,8 @@ public class PerfumeStoryRequest implements Serializable {
 
     private String styleAnswer;
 
+    public PerfumeStoryRequest(){}
+
     @Builder
     public PerfumeStoryRequest(String genderAnswer, String scentAnswer, String moodAnswer, String seasonAnswer, String styleAnswer) {
         this.genderAnswer = genderAnswer;
@@ -24,5 +26,15 @@ public class PerfumeStoryRequest implements Serializable {
         this.moodAnswer = moodAnswer;
         this.seasonAnswer = seasonAnswer;
         this.styleAnswer = styleAnswer;
+    }
+
+    public String toPromptString() {
+        return "제가 제시할 단어는 다음과 같습니다. " +
+                "성별: " + genderAnswer +
+                ", 향수 노트: " + scentAnswer +
+                ", 분위기: " + moodAnswer +
+                ", 계절: " + seasonAnswer +
+                ", 스타일: " + styleAnswer + "." +
+                "해당 5가지의 단어들을 조합하여 400글자 이내의 짧은 스토리를 만들어주세요";
     }
 }
