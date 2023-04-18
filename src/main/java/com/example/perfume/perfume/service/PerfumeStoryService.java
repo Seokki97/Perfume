4441e276538ab9910484ec2d01efe62db8dd0fbd
JunSeok Kim <skaksdl1238@gmail.com>
2023-04-18 16:32:10 +0900

    feat - Exception의 경우 메서드 추가, 생성자 메서드 분리

--- src/main/java/com/example/perfume/perfume/service/PerfumeStoryService.java
+++ src/main/java/com/example/perfume/perfume/service/PerfumeStoryService.java
@@ -4,6 +4,7 @@ import com.example.perfume.config.ChatGptConfig;
 import com.example.perfume.perfume.dto.story.ChatGptRequest;
 import com.example.perfume.perfume.dto.story.ChatGptResponse;
 import com.example.perfume.perfume.dto.story.PerfumeStoryRequest;
+import com.example.perfume.perfume.exception.GptNotResponseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,18 +16,21 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class PerfumeStoryService {
     @Value("${gpt.apiKey}")
-    private String API_KEY;
-    private static RestTemplate restTemplate = new RestTemplate();
-
+    private String MY_KEY;
 
     public HttpEntity<ChatGptRequest> createHttpEntity(ChatGptRequest chatGptRequest) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE));
-        headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + API_KEY);
+        headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + MY_KEY);
         return new HttpEntity<>(chatGptRequest, headers);
     }
 
+    //해당 부분을 post맨으로 request 보내보기
+    //만약 gpt 에서 null이 들어오는거면 내가 데이터 보내는 것이 잘못됐을수도
+    //gpt에서 데이터를 잘 보내주는데 응답으로 null이 들어오는거면 내가 데이터 파싱을 잘못한걸수도
     public ChatGptResponse getResponse(HttpEntity<ChatGptRequest> chatGptRequest) {
+        RestTemplate restTemplate = new RestTemplate();
+
         ResponseEntity<ChatGptResponse> responseEntity = restTemplate.postForEntity(
                 ChatGptConfig.URL,
                 chatGptRequest,
@@ -35,14 +39,32 @@ public class PerfumeStoryService {
     }
 
     public ChatGptResponse askQuestionToChatGpt(PerfumeStoryRequest perfumeStoryRequest) {
-        return this.getResponse(
-                this.createHttpEntity(
-                        ChatGptRequest.builder()
-                                .model(ChatGptConfig.MODEL)
-                                .prompt(perfumeStoryRequest.toPromptString())
-                                .maxToken(ChatGptConfig.MAX_TOKEN)
-                                .temperature(ChatGptConfig.TEMPERATURE)
-                                .topP(ChatGptConfig.TOP_P)
-                                .build()));
+        ChatGptRequest chatGptRequest = makeGptRequest(perfumeStoryRequest);
+
+        ChatGptResponse chatGptResponse = getResponse(this.createHttpEntity(chatGptRequest));
+
+        if (isAnswerNull(chatGptResponse)) {
+            throw new GptNotResponseException();
+        }
+
+        return chatGptResponse;
     }
+
+    private ChatGptRequest makeGptRequest(PerfumeStoryRequest perfumeStoryRequest) {
+        return ChatGptRequest.builder()
+                .model(ChatGptConfig.MODEL)
+                .prompt(perfumeStoryRequest.toPromptString())
+                .maxToken(ChatGptConfig.MAX_TOKEN)
+                .temperature(ChatGptConfig.TEMPERATURE)
+                .topP(ChatGptConfig.TOP_P)
+                .build();
+    }
+    private boolean isAnswerNull(ChatGptResponse chatGptResponse) {
+        if (chatGptResponse.getChoiceList() == null) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
