26c55a50adeb1d46c59a06fc1a4e1f540e6a6182
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-02 18:41:52 +0900

    refactor - import 제거

--- src/main/java/com/example/perfume/perfume/controller/docs/PerfumeControllerDocs.java
+++ src/main/java/com/example/perfume/perfume/controller/docs/PerfumeControllerDocs.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
