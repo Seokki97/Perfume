4f10c90be562698d6c61b0bb28e36a5b5a5702d8
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-03 11:31:33 +0900

    feat - WishList Dto 생성

--- src/main/java/com/example/perfume/wishlist/dto/WishListRequest.java
+++ src/main/java/com/example/perfume/wishlist/dto/WishListRequest.java
@@ -1,7 +1,26 @@
 package com.example.perfume.wishlist.dto;
 
+import com.example.perfume.member.domain.Member;
+import com.example.perfume.perfume.domain.Perfume;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class WishListRequest {
+
+    private Long wishListId;
+
+    private Member member;
+
+    private Perfume perfume;
+
+    public WishListRequest() {
+    }
+
+    @Builder
+    public WishListRequest(Long wishListId, Member member, Perfume perfume) {
+        this.wishListId = wishListId;
+        this.member = member;
+        this.perfume = perfume;
+    }
 }
--- src/main/java/com/example/perfume/wishlist/dto/WishListResponse.java
+++ src/main/java/com/example/perfume/wishlist/dto/WishListResponse.java
@@ -1,7 +1,26 @@
 package com.example.perfume.wishlist.dto;
 
+import com.example.perfume.member.domain.Member;
+import com.example.perfume.perfume.domain.Perfume;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class WishListResponse {
+
+    private Long wishListId;
+
+    private Member member;
+
+    private Perfume perfume;
+
+    public WishListResponse() {
+    }
+
+    @Builder
+    public WishListResponse(Long wishListId, Member member, Perfume perfume) {
+        this.wishListId = wishListId;
+        this.member = member;
+        this.perfume = perfume;
+    }
 }
