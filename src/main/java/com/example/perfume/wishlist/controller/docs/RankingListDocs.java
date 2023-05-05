bdd42f5b24832c47a8ec956c0829a924d918b51d
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-05 17:12:47 +0900

    feat - Ranking산정 Api 스웨거 생성

---
+++ src/main/java/com/example/perfume/wishlist/controller/docs/RankingListDocs.java
@@ -0,0 +1,20 @@
+package com.example.perfume.wishlist.controller.docs;
+
+import com.example.perfume.wishlist.dto.RankingResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Tag(name = "위시리스트에 담긴 항목들 랭킹 산정 Api")
+public interface RankingListDocs {
+    @Operation(summary = "위시리스트 항목들의 랭킹을 매겨준다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "성공"),
+            @ApiResponse(responseCode = "404",description = "랭킹을 산정할 수 없습니다")
+    })
+    ResponseEntity<List<RankingResponse>> showRanking();
+}
