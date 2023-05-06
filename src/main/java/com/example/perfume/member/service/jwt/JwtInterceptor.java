25ef0f07637c0bac0b34589432aebb5cbb1f0f81
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-06 14:21:58 +0900

    refactor - import 제거

--- src/main/java/com/example/perfume/member/service/jwt/JwtInterceptor.java
+++ src/main/java/com/example/perfume/member/service/jwt/JwtInterceptor.java
@@ -1,20 +1,13 @@
 package com.example.perfume.member.service.jwt;
 
 
-import com.example.perfume.advice.BadRequestException;
-import com.example.perfume.member.exception.TokenExpiredException;
 import com.example.perfume.member.exception.TokenInvalidException;
-import com.example.perfume.member.exception.UserNotFoundException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
