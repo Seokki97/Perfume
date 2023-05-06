ecd82639d871758f0499478a37a008f64d3c4352
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-06 20:14:06 +0900

    refactor - STORY 필드 제거

--- src/main/java/com/example/perfume/crawling/domain/perfume/PerfumeType.java
+++ src/main/java/com/example/perfume/crawling/domain/perfume/PerfumeType.java
@@ -1,7 +1,7 @@
 package com.example.perfume.crawling.domain.perfume;
 
 public enum PerfumeType {
-    NAME(0),FEATURE(1),BRAND(2),IMAGE(3),STORY(4);
+    NAME(0),FEATURE(2),BRAND(1),IMAGE(3);
     private int perfumeValue;
 
     PerfumeType(int perfumeValue) {
