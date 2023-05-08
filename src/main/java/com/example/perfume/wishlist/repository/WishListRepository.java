d64f152e98fd285a9c6de4111126120002ea88c7
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-08 18:09:40 +0900

    refactor - Optional -> void 타입 변경, existsWishList 추가

--- src/main/java/com/example/perfume/wishlist/repository/WishListRepository.java
+++ src/main/java/com/example/perfume/wishlist/repository/WishListRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
     List<WishList> findByMemberId(Long memberId);
 
-    List<WishList> deleteByMemberId(Long memberId);
+    void deleteByMemberId(Long memberId);
 
-    Optional<WishList> deleteByMemberIdAndPerfumeId(Long memberId, Long perfumeId);
+    void deleteByMemberIdAndPerfumeId(Long memberId, Long perfumeId);
+
+    boolean existsByMemberIdAndPerfumeId(Long memberId, Long perfumeId);
 
     List<WishList> findAll();
 }
