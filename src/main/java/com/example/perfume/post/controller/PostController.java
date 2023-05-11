65574c0ad05a72edde7a9945f08a4bf1e7a338a2
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:08:27 +0900

    feat - PostController Logger 적용

--- src/main/java/com/example/perfume/post/controller/PostController.java
+++ src/main/java/com/example/perfume/post/controller/PostController.java
@@ -4,11 +4,13 @@ import com.example.perfume.post.controller.docs.PostControllerDocs;
 import com.example.perfume.post.dto.PostRequestDto;
 import com.example.perfume.post.domain.Post;
 import com.example.perfume.post.service.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/post")
 public class PostController implements PostControllerDocs {
@@ -21,17 +23,20 @@ public class PostController implements PostControllerDocs {
 
     @PostMapping("/save-post")
     public ResponseEntity<Void> savePost(@RequestBody PostRequestDto postRequestDto) {
+        log.info("방명록 작성하기 요청");
         postService.savePost(postRequestDto);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/show-post/{id}")
     public ResponseEntity<Post> showOnePost(@PathVariable Long id) {
+        log.info("게시글 넘버 :{} 조회하기", id);
         return ResponseEntity.ok(postService.showOnePost(id));
     }
 
     @GetMapping("/show-all-post")
     public ResponseEntity<List<Post>> showAllPost() {
+        log.info("모든 게시글 조회 요청");
         return ResponseEntity.ok(postService.showAllPost());
     }
 }
