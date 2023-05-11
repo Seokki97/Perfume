deb3697a7ae3bd7bdf49b5bb1c72ac208179950a
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:07:51 +0900

    feat - OauthController Logger 적용

--- src/main/java/com/example/perfume/oauth/controller/OauthController.java
+++ src/main/java/com/example/perfume/oauth/controller/OauthController.java
@@ -2,15 +2,16 @@ package com.example.perfume.oauth.controller;
 
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.member.dto.loginDto.LoginResponse;
-import com.example.perfume.member.dto.memberDto.MemberRequestDto;
 import com.example.perfume.member.service.LoginService;
 import com.example.perfume.oauth.controller.docs.OauthControllerDocs;
 import com.example.perfume.oauth.service.OauthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @RestController
 @RequestMapping("/oauth")
 public class OauthController implements OauthControllerDocs {
@@ -27,8 +28,7 @@ public class OauthController implements OauthControllerDocs {
     @GetMapping("/login")
     public ResponseEntity<LoginResponse> signUp(@RequestParam String code, HttpSession httpSession) {
         Member member = oauthService.loadUserProfile(code, httpSession);
-
+        log.info("카카오에서 받은 회원 Id : {} 회원가입 요청",member.getId());
         return ResponseEntity.ok(loginService.generateToken(member.getId()));
     }
-
 }
