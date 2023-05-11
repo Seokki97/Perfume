27f9e17006c3f8705f83eeaaa1abee6da2f985ea
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:08:16 +0900

    feat - PerfumeStoryController Logger 적용

--- src/main/java/com/example/perfume/perfume/controller/PerfumeStoryController.java
+++ src/main/java/com/example/perfume/perfume/controller/PerfumeStoryController.java
@@ -1,20 +1,17 @@
 package com.example.perfume.perfume.controller;
 
-import com.example.perfume.config.ChatGptConfig;
-import com.example.perfume.perfume.controller.docs.PerfumeControllerDocs;
 import com.example.perfume.perfume.controller.docs.PerfumeStoryControllerDocs;
-import com.example.perfume.perfume.dto.story.ChatGptRequest;
 import com.example.perfume.perfume.dto.story.ChatGptResponse;
 import com.example.perfume.perfume.dto.story.PerfumeStoryRequest;
 import com.example.perfume.perfume.service.PerfumeStoryService;
-import org.apache.catalina.connector.Response;
-import org.springframework.http.HttpEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/perfume")
 public class PerfumeStoryController implements PerfumeStoryControllerDocs {
@@ -26,7 +23,7 @@ public class PerfumeStoryController implements PerfumeStoryControllerDocs {
 
     @PostMapping("/make-story")
     public ResponseEntity<ChatGptResponse> makePerfumeStory(@RequestBody PerfumeStoryRequest perfumeStoryRequest) {
-
+        log.info("Chat GPT에게 향수 스토리 생성 요청, 질문 내용 : {}",perfumeStoryRequest.toPromptString());
         return ResponseEntity.ok(perfumeStoryService.askQuestionToChatGpt(perfumeStoryRequest));
     }
 
