eb3ce233b252232ae2beae0692b82106fbfdc63c
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:09:05 +0900

    feat - SimilarPerfumeController Logger 적용

--- src/main/java/com/example/perfume/survey/controller/SimilarPerfumeController.java
+++ src/main/java/com/example/perfume/survey/controller/SimilarPerfumeController.java
@@ -1,9 +1,9 @@
 package com.example.perfume.survey.controller;
 
-import com.example.perfume.log.LoggerUtil;
 import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.survey.controller.docs.SimilarPerfumeControllerDocs;
 import com.example.perfume.survey.service.SimilarPerfumeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/survey")
 public class SimilarPerfumeController implements SimilarPerfumeControllerDocs {
-
     private final SimilarPerfumeService similarPerfumeService;
 
     public SimilarPerfumeController(SimilarPerfumeService similarPerfumeService) {
@@ -24,7 +24,7 @@ public class SimilarPerfumeController implements SimilarPerfumeControllerDocs {
 
     @GetMapping("/show-similar-perfume/{id}")
     public ResponseEntity<List<Perfume>> showSimilarData(@PathVariable("id") Long id) {
-        LoggerUtil.countSecondApi("비슷한 향수 추천 (두번째 기능) API 호출 횟수: ");
+        log.info("유사한 향수 조회 Perfume Id : {}", id);
         return ResponseEntity.ok(similarPerfumeService.showSimilarPerfume(id));
     }
 }
