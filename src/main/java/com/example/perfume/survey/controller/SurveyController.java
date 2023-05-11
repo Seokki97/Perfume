6d2c0df679744f30ced4cb5fce9dcb80d4c3366d
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:09:14 +0900

    feat - SurveyController Logger 적용

--- src/main/java/com/example/perfume/survey/controller/SurveyController.java
+++ src/main/java/com/example/perfume/survey/controller/SurveyController.java
@@ -1,18 +1,19 @@
 package com.example.perfume.survey.controller;
 
 import com.example.perfume.crawling.domain.survey.SurveyList;
-import com.example.perfume.log.LoggerUtil;
 import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.survey.controller.docs.SurveyControllerDocs;
 import com.example.perfume.survey.dto.surveyDto.SurveyRequestDto;
 import com.example.perfume.survey.service.DataService;
 import com.example.perfume.survey.service.SurveyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/survey")
 public class SurveyController implements SurveyControllerDocs {
@@ -33,7 +34,12 @@ public class SurveyController implements SurveyControllerDocs {
 
     @PostMapping("/show-perfume-by-survey")
     public ResponseEntity<List<Perfume>> showPerfumeDataBySurvey(@RequestBody SurveyRequestDto surveyRequestDto) {
-        LoggerUtil.countFirstApi("나에게 잘 어울리는 향수 (첫번째 기능) API 호출 횟수 : ");
+        log.info("설문 기반의 향수 찾기 Gender : {}, Scent : {}, Mood : {}, Season : {}, Style : {}",
+                surveyRequestDto.getGenderAnswer(),
+                surveyRequestDto.getScentAnswer(),
+                surveyRequestDto.getMoodAnswer(),
+                surveyRequestDto.getSeasonAnswer(),
+                surveyRequestDto.getStyleAnswer());
         return ResponseEntity.ok(surveyService.showPerfumeListBySurvey(surveyRequestDto));
     }
 
