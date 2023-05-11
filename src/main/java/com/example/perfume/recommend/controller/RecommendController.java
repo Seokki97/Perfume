fc0cdacc2117ef557b157195b2fb7ea426666fe4
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:08:54 +0900

    feat - RecommendController Logger 적용

--- src/main/java/com/example/perfume/recommend/controller/RecommendController.java
+++ src/main/java/com/example/perfume/recommend/controller/RecommendController.java
@@ -1,6 +1,5 @@
 package com.example.perfume.recommend.controller;
 
-import com.example.perfume.log.LoggerUtil;
 import com.example.perfume.member.service.LogoutService;
 import com.example.perfume.member.service.jwt.LoginCheck;
 import com.example.perfume.recommend.controller.docs.RecommendControllerDocs;
@@ -8,14 +7,17 @@ import com.example.perfume.recommend.domain.Recommendation;
 import com.example.perfume.recommend.dto.RecommendRequestDto;
 import com.example.perfume.recommend.dto.RecommendResponseDto;
 import com.example.perfume.recommend.service.RecommendationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 @RequestMapping("/member")
 public class RecommendController implements RecommendControllerDocs {
+
     private final RecommendationService recommendationService;
     private final LogoutService logoutService;
 
@@ -26,13 +28,14 @@ public class RecommendController implements RecommendControllerDocs {
 
     @PostMapping("/recommend/{id}")
     public ResponseEntity<Recommendation> recommendPerfume(@PathVariable("id") Long id, @RequestBody RecommendRequestDto recommendRequestDto) {
+        log.info("memberId : {}에게 향수를 추천해주는 요청",id);
         return ResponseEntity.ok(recommendationService.recommendByOtherGuest(id, recommendRequestDto));
     }
 
     @LoginCheck
     @GetMapping("/show-recommended-perfume/{id}")
     public ResponseEntity<RecommendResponseDto> showRecommendedPerfume(@PathVariable("id") Long id, HttpServletRequest httpServletRequest) {
-        LoggerUtil.countThirdApi("타인이 보는 나의 향수 (세번째 기능) API 호출 횟수 :");
+        log.info("memberId : {}에게 추천된 향수를 조회하기", id);
         logoutService.isUserAlreadyLogout(httpServletRequest.getHeader("Authorization"));
         return ResponseEntity.ok(recommendationService.showRecommendedPerfume(id));
     }
