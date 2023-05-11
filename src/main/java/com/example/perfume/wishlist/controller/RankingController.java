f5d2ad074027fd55f58fae5dd867af7fd642bd8c
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:08:40 +0900

    feat - RankingController Logger 적용

--- src/main/java/com/example/perfume/wishlist/controller/RankingController.java
+++ src/main/java/com/example/perfume/wishlist/controller/RankingController.java
@@ -3,6 +3,7 @@ package com.example.perfume.wishlist.controller;
 import com.example.perfume.wishlist.controller.docs.RankingListDocs;
 import com.example.perfume.wishlist.dto.RankingResponse;
 import com.example.perfume.wishlist.service.WishListAnalyze;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/member/wish")
 public class RankingController implements RankingListDocs {
@@ -21,6 +23,7 @@ public class RankingController implements RankingListDocs {
 
     @GetMapping("/show-ranking")
     public ResponseEntity<List<RankingResponse>> showRanking() {
+        log.info("WishList 랭킹 조회하기 요청");
         return ResponseEntity.ok(wishListAnalyze.showTopRankedPerfumeList());
     }
 
