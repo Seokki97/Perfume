047a5e222c727e0c279d4e58081a17a1046a480f
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-11 14:07:07 +0900

    feat - AnalyzeController Logger 적용

--- src/main/java/com/example/perfume/recommend/controller/AnalyzeController.java
+++ src/main/java/com/example/perfume/recommend/controller/AnalyzeController.java
@@ -3,6 +3,7 @@ package com.example.perfume.recommend.controller;
 import com.example.perfume.member.service.jwt.LoginCheck;
 import com.example.perfume.recommend.controller.docs.AnalyzeControllerDocs;
 import com.example.perfume.recommend.service.analyze.AnalyzeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/member")
 public class AnalyzeController implements AnalyzeControllerDocs {
@@ -24,7 +26,7 @@ public class AnalyzeController implements AnalyzeControllerDocs {
     @LoginCheck
     @GetMapping("/show-result/{id}")
     public ResponseEntity<Map<String, Long>> showMostRecommendedPerfume(@PathVariable("id") Long memberId) {
-
+        log.info("memberId : {} 의 추천받은 향수 결과 분석 요청", memberId);
         return ResponseEntity.ok(analyzeService.showAnalyzedData(memberId));
     }
 }
