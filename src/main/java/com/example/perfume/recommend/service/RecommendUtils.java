12e214cf5aaf51b6f6e1d888e3a901f3e0d3fcf6
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-22 15:28:49 +0900

    fix - 추천된 향수가 없을 경우 예외

--- src/main/java/com/example/perfume/recommend/service/RecommendUtils.java
+++ src/main/java/com/example/perfume/recommend/service/RecommendUtils.java
@@ -1,6 +1,7 @@
 package com.example.perfume.recommend.service;
 
 import com.example.perfume.perfume.domain.Perfume;
+import com.example.perfume.recommend.exception.RecommendNotFoundException;
 
 import java.util.List;
 import java.util.Random;
@@ -9,10 +10,15 @@ public class RecommendUtils {
 
     public static int createRandomPerfumeFromList(List<Perfume> surveyResultList) {
         Random random = new Random();
-        return random.nextInt(surveyResultList.size());
+        int recommendedPerfumeSize = surveyResultList.size();
+
+        if(recommendedPerfumeSize == 0){
+           throw new RecommendNotFoundException();
+        }
+        return random.nextInt(recommendedPerfumeSize);
     }
 
-    public static int createRandomNumber(List<String> nicknameList){
+    public static int createRandomNumber(List<String> nicknameList) {
         Random random = new Random();
         int nicknameListSize = nicknameList.size();
         return random.nextInt(nicknameListSize);
