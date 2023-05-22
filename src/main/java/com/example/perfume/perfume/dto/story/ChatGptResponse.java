fa9dc4efd6cab40fe5c29a41e4640c0c2c4a7203
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-22 18:00:58 +0900

    feat - text-davinci-003 -> gpt-3.5-turbo 모델로 전환

--- src/main/java/com/example/perfume/perfume/dto/story/ChatGptResponse.java
+++ src/main/java/com/example/perfume/perfume/dto/story/ChatGptResponse.java
@@ -1,7 +1,9 @@
 package com.example.perfume.perfume.dto.story;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
@@ -14,14 +16,16 @@ public class ChatGptResponse implements Serializable {
     private LocalDate created;
     private String model;
     private List<Choice> choices;
+    private Usage usage;
 
 
     @Builder
-    public ChatGptResponse(String id, String object,LocalDate created, String model, List<Choice> choices) {
+    public ChatGptResponse(String id, String object, LocalDate created, String model, List<Choice> choices, Usage usage) {
         this.id = id;
         this.object = object;
         this.created = created;
         this.choices = choices;
         this.model = model;
+        this.usage = usage;
     }
 }
