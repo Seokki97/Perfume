ea4e24874809fe2f6a4f3a345a83ace9ffbd24be
JunSeok Kim <skaksdl1238@gmail.com>
2023-05-22 18:01:10 +0900

    feat - text-davinci-003 -> gpt-3.5-turbo 모델로 전환

--- src/main/java/com/example/perfume/perfume/dto/story/ChatGptRequest.java
+++ src/main/java/com/example/perfume/perfume/dto/story/ChatGptRequest.java
@@ -3,27 +3,25 @@ package com.example.perfume.perfume.dto.story;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 @Getter
 public class ChatGptRequest implements Serializable {
 
     private String model;
-    private String prompt;
-    @JsonProperty("max_tokens")
-    private Integer maxTokens;
-    private Double temperature;
-    @JsonProperty("top_p")
-    private Double topP;
 
-    public ChatGptRequest(){}
+    private JSONArray messages;
+
+    public ChatGptRequest() {
+    }
+
     @Builder
-    public ChatGptRequest(String model, String prompt, Integer maxTokens, Double temperature, Double topP) {
+    public ChatGptRequest(String model, JSONArray messages) {
         this.model = model;
-        this.prompt = prompt;
-        this.maxTokens = maxTokens;
-        this.temperature = temperature;
-        this.topP = topP;
+        this.messages = messages;
     }
 }
