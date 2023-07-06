4fe500aeb0a350f3fe76f8b4b2b54568ce5ff02b
JunSeok Kim <skaksdl1238@gmail.com>
2023-07-06 18:01:58 +0900

    feat - 리뷰게시판 Dto 생성

---
+++ src/main/java/com/example/perfume/board/dto/responseDto/ReviewBoardResponse.java
@@ -0,0 +1,32 @@
+package com.example.perfume.board.dto.responseDto;
+
+import com.example.perfume.board.domain.Content;
+import com.example.perfume.member.domain.Member;
+import com.example.perfume.perfume.domain.Perfume;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ReviewBoardResponse {
+
+    private Long boardId;
+
+    private Member writer;
+
+    private String title;
+
+    private Content content;
+
+    private Perfume perfume;
+
+
+    @Builder
+    public ReviewBoardResponse(final Long boardId, final Member member, final String title,
+                              final Content content, final Perfume perfume) {
+        this.boardId = boardId;
+        this.writer = member;
+        this.content = content;
+        this.title = title;
+        this.perfume = perfume;
+    }
+}
