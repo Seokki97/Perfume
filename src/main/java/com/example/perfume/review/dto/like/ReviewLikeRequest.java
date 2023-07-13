6ab7716a3e76a2352c08702f65a089bd9f283bea
JunSeok Kim <skaksdl1238@gmail.com>
2023-07-13 09:11:45 +0900

    feat - toEntity 분리

--- src/main/java/com/example/perfume/review/dto/like/ReviewLikeRequest.java
+++ src/main/java/com/example/perfume/review/dto/like/ReviewLikeRequest.java
@@ -2,21 +2,32 @@ package com.example.perfume.review.dto.like;
 
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.review.domain.like.LikeStatus;
+import com.example.perfume.review.domain.like.ReviewLike;
 import com.example.perfume.review.domain.review.PerfumeReviewBoard;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ReviewLikeRequest {
 
-    private Member memberId;
+    private Member member;
 
-    private PerfumeReviewBoard postId;
+    private PerfumeReviewBoard post;
 
     private LikeStatus likeStatus;
 
+    @Builder
     public ReviewLikeRequest(Member memberId, PerfumeReviewBoard postId, LikeStatus likeStatus) {
-        this.memberId = memberId;
-        this.postId = postId;
+        this.member = memberId;
+        this.post = postId;
         this.likeStatus = likeStatus;
+    }
+
+    public ReviewLike toEntity() {
+        return ReviewLike.builder()
+                .likedPost(post)
+                .likeStatus(likeStatus)
+                .member(member)
+                .build();
     }
 }
