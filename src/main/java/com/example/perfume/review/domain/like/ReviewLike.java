431f1fd3797f3ebd128a6e96d0a291507d70f67c
JunSeok Kim <skaksdl1238@gmail.com>
2023-07-13 09:13:01 +0900

    feat - 도메인 like,unlike기능 생성, 취소기능 간소화

--- src/main/java/com/example/perfume/review/domain/like/ReviewLike.java
+++ src/main/java/com/example/perfume/review/domain/like/ReviewLike.java
@@ -2,6 +2,8 @@ package com.example.perfume.review.domain.like;
 
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.review.domain.review.PerfumeReviewBoard;
+import com.example.perfume.review.dto.like.ReviewLikeRequest;
+import com.example.perfume.review.exception.ReviewLikeInvalidException;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +18,7 @@ public class ReviewLike {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_id", nullable = false)
-    private Long review_id;
+    private Long reviewId;
 
     @ManyToOne
     private Member member;
@@ -24,28 +26,44 @@ public class ReviewLike {
     @ManyToOne
     private PerfumeReviewBoard likedPost;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.ORDINAL)
     private LikeStatus likeStatus;
 
     @Builder
-    public ReviewLike(final Long review_id, final Member member, final PerfumeReviewBoard likedPost) {
-        this.review_id = review_id;
+    public ReviewLike(final Long reviewId, final Member member, final PerfumeReviewBoard likedPost, LikeStatus likeStatus) {
+        this.reviewId = reviewId;
         this.member = member;
         this.likedPost = likedPost;
+        this.likeStatus = likeStatus;
     }
 
+
     public void updateLike() {
-        if (this.likeStatus != LikeStatus.LIKE) {
+        if (this.likeStatus == LikeStatus.UNLIKE) {
+            this.likedPost.decreaseUnlikeCount();
+
+        } else if (this.likeStatus == LikeStatus.LIKE) {
+            this.likeStatus = LikeStatus.CANCELED;
+            likedPost.decreaseLikeCount();
+
+        } else {
             this.likeStatus = LikeStatus.LIKE;
             this.likedPost.increaseLikeCount();
         }
     }
 
-    public void updateUnlike() {
-        if (this.likeStatus != LikeStatus.CANCELED) {
-            this.likeStatus = LikeStatus.CANCELED;
+    public void updateUnLike() {
+        if (this.likeStatus == LikeStatus.LIKE) {
             this.likedPost.decreaseLikeCount();
+
+        } else if (this.likeStatus == LikeStatus.UNLIKE) {
+            this.likeStatus = LikeStatus.CANCELED;
+            likedPost.decreaseUnlikeCount();
+
+        } else {
+            this.likeStatus = LikeStatus.UNLIKE;
+            this.likedPost.increaseUnlikeCount();
+
         }
     }
-
 }
