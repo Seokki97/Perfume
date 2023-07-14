0e54c248699c5eb7cf41c172d69e049b51cea0f8
JunSeok Kim <skaksdl1238@gmail.com>
2023-07-14 13:21:29 +0900

    refactor -  트랜잭션 어노테이션 추가

--- src/main/java/com/example/perfume/review/service/ReviewLikeService.java
+++ src/main/java/com/example/perfume/review/service/ReviewLikeService.java
@@ -2,36 +2,52 @@ package com.example.perfume.review.service;
 
 
 import com.example.perfume.review.domain.like.ReviewLike;
+import com.example.perfume.review.domain.review.PerfumeReviewBoard;
 import com.example.perfume.review.dto.like.ReviewLikeRequest;
+import com.example.perfume.review.exception.ReviewPostNotFoundException;
+import com.example.perfume.review.repository.ReviewBoardRepository;
 import com.example.perfume.review.repository.ReviewLikeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewLikeService {
 
     private final ReviewLikeRepository reviewLikeRepository;
 
-    public ReviewLikeService(ReviewLikeRepository reviewLikeRepository) {
+    private final ReviewBoardRepository reviewBoardRepository;
+
+    public ReviewLikeService(ReviewLikeRepository reviewLikeRepository, ReviewBoardRepository reviewBoardRepository) {
         this.reviewLikeRepository = reviewLikeRepository;
+        this.reviewBoardRepository = reviewBoardRepository;
     }
 
+    @Transactional
     public void likePost(ReviewLikeRequest reviewLikeRequest) {
         ReviewLike reviewLike = reviewLikeRequest.toEntity();
+
+        PerfumeReviewBoard perfumeReviewBoard = reviewBoardRepository.findByBoardId(reviewLike.getLikedPost().getBoardId())
+                .orElseThrow(ReviewPostNotFoundException::new);
 
         reviewLike.updateLike();
         if (!isAlreadyPushLikeOrUnlike(reviewLikeRequest)) {
             reviewLikeRepository.save(reviewLike);
         }
+        perfumeReviewBoard.likePost(reviewLike);
     }
 
+    @Transactional
     public void unlikePost(ReviewLikeRequest reviewLikeRequest) {
         ReviewLike reviewLike = reviewLikeRequest.toEntity();
 
-        reviewLike.updateUnLike();
+        PerfumeReviewBoard perfumeReviewBoard = reviewBoardRepository.findByBoardId(reviewLike.getLikedPost().getBoardId())
+                .orElseThrow(ReviewPostNotFoundException::new);
 
+        reviewLike.updateUnLike();
         if (!isAlreadyPushLikeOrUnlike(reviewLikeRequest)) {
             reviewLikeRepository.save(reviewLike);
         }
+        perfumeReviewBoard.unlikePost(reviewLike);
     }
 
     public boolean isAlreadyPushLikeOrUnlike(ReviewLikeRequest reviewLikeRequest) {
