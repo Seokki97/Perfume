2c0c435e0cf78f0aca3e78962028fb7ad1f842f4
JunSeok Kim <skaksdl1238@gmail.com>
2023-08-24 11:46:49 +0900

    refactor - Long->Object 변경

--- src/main/java/com/example/perfume/email/MailDto.java
+++ src/main/java/com/example/perfume/email/MailDto.java
@@ -1,5 +1,6 @@
 package com.example.perfume.email;
 
+import com.example.perfume.review.domain.report.Report;
 import com.example.perfume.review.dto.report.requestDto.ReportRequest;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +20,7 @@ public class MailDto {
 
     public static MailDto generateReportMail(ReportRequest reportRequest){
         return MailDto.builder()
-                .to(reportRequest.getReportDetail().getAdmin().getEmail())
+                .to(reportRequest.getReportDetail().getAdministrator().getEmail())
                 .subject(reportRequest.getReportType().name()+"에 관한 신고내용 입니다.")
                 .message("신고 내용 입니다\n"+reportRequest.getReportDetail())
                 .build();
--- src/main/java/com/example/perfume/review/domain/report/ReportDetail.java
+++ src/main/java/com/example/perfume/review/domain/report/ReportDetail.java
@@ -3,11 +3,14 @@ package com.example.perfume.review.domain.report;
 import com.example.perfume.member.domain.Member;
 import com.example.perfume.review.domain.review.PerfumeReviewBoard;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
 
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Getter
@@ -15,13 +18,15 @@ import javax.persistence.ManyToOne;
 @Embeddable
 public class ReportDetail {
 
-    private Long administrator;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member administrator;
 
     private Long postId;
 
     private Long reportedPostUserId;
 
-    public ReportDetail(Long administrator, Long postId, Long reportedPostUserId) {
+    @Builder
+    public ReportDetail(Member administrator, Long postId, Long reportedPostUserId) {
         this.administrator = administrator;
         this.postId = postId;
         this.reportedPostUserId = reportedPostUserId;
