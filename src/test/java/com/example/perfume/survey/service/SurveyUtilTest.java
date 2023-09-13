d05e73e5422651c22b73416359d66082a21e55c6
JunSeok Kim <skaksdl1238@gmail.com>
2023-09-13 18:57:01 +0900

    test - SurveyList->PerfumeList 테스트코드 작성

--- src/test/java/com/example/perfume/survey/service/SurveyUtilTest.java
+++ src/test/java/com/example/perfume/survey/service/SurveyUtilTest.java
@@ -1,12 +1,18 @@
 package com.example.perfume.survey.service;
 
+import com.example.perfume.perfume.domain.Perfume;
 import com.example.perfume.survey.domain.Survey;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,5 +35,21 @@ public class SurveyUtilTest {
                         result -> assertThat(result).isEqualTo(actualCaseOne),
                         result -> assertThat(result).isEqualTo(actualCaseTwo)
                 );
+    }
+
+    @DisplayName("SurveyList에서 Perfume객체를 추출한다")
+    @Test
+    void extractPerfumeData() {
+        List<Survey> surveyList = new ArrayList<>();
+
+        List<Perfume> perfumeList = new ArrayList<>();
+
+        perfumeList.add(new Perfume(1l, null, null, null, null, null));
+
+        when(surveyUtil.convertToPerfumeData(anyList())).thenReturn(perfumeList);
+
+        List<Perfume> actual = surveyUtil.convertToPerfumeData(surveyList);
+
+        Assertions.assertEquals(1l, actual.get(0).getId());
     }
 }
