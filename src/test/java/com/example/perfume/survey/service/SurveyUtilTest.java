274a41e386abb5e22283a2dbb209cc85abb5e3b0
JunSeok Kim <skaksdl1238@gmail.com>
2023-09-14 17:06:18 +0900

    refactor - 불필요한 Mockito 주입 제거

--- src/test/java/com/example/perfume/survey/service/SurveyUtilTest.java
+++ src/test/java/com/example/perfume/survey/service/SurveyUtilTest.java
@@ -5,28 +5,27 @@ import com.example.perfume.survey.domain.Survey;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class SurveyUtilTest {
 
-    private final SurveyUtil surveyUtil = mock(SurveyUtil.class);
+    @Autowired
+    private SurveyUtil surveyUtil;
 
     @DisplayName("무드에 대한 답변을 랜덤으로 선택한다.")
     @Test
     void getRandomMoodAnswer() {
-        Survey survey = mock(Survey.class);
-        String moodAnswer = "달콤한";
-        when(surveyUtil.showMoodAnswer(survey)).thenReturn(moodAnswer);
+        Survey survey = Survey.builder()
+                .moodAnswer("달콤한 상큼한")
+                .build();
 
         String actualCaseOne = "달콤한";
         String actualCaseTwo = "상큼한";
@@ -40,16 +39,41 @@ public class SurveyUtilTest {
     @DisplayName("SurveyList에서 Perfume객체를 추출한다")
     @Test
     void extractPerfumeData() {
+        Perfume perfume = Perfume.builder()
+                .id(1l).build();
+
+        Survey survey = Survey.builder()
+                .perfume(perfume)
+                .build();
+
         List<Survey> surveyList = new ArrayList<>();
-
-        List<Perfume> perfumeList = new ArrayList<>();
-
-        perfumeList.add(new Perfume(1l, null, null, null, null, null));
-
-        when(surveyUtil.convertToPerfumeData(anyList())).thenReturn(perfumeList);
+        surveyList.add(survey);
 
         List<Perfume> actual = surveyUtil.convertToPerfumeData(surveyList);
 
-        Assertions.assertEquals(1l, actual.get(0).getId());
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1l, actual.get(0).getId()),
+                () -> Assertions.assertEquals(perfume, actual.get(0))
+        );
+    }
+
+    @DisplayName("SurveyList가 비어있는지 검증한다")
+    @Test
+    void isEmptySurveyList() {
+        List<Survey> emptyList = Collections.emptyList();
+
+        Survey survey = Survey.builder()
+                .build();
+
+        List<Survey> surveyList = new ArrayList<>();
+        surveyList.add(survey);
+
+        boolean trueAnswer = surveyUtil.isEmptyRecommendedPerfumeList(emptyList);
+        boolean falseAnswer = surveyUtil.isEmptyRecommendedPerfumeList(surveyList);
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(trueAnswer),
+                () -> Assertions.assertFalse(falseAnswer)
+        );
     }
 }
