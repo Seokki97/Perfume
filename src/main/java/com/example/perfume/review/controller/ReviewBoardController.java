2922be7bc6512c8977af7ae3ce630534c4fd364a
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-24 20:43:14 +0900

    refactor - ReviewBoardController Logging 적용

--- src/main/java/com/example/perfume/review/controller/ReviewBoardController.java
+++ src/main/java/com/example/perfume/review/controller/ReviewBoardController.java
@@ -8,11 +8,13 @@ import com.example.perfume.review.dto.review.requestDto.ReviewBoardRequest;
 import com.example.perfume.review.dto.review.responseDto.ReviewBoardResponse;
 import com.example.perfume.review.service.ReviewBoardService;
 import com.example.perfume.member.service.jwt.LoginCheck;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/board")
 public class ReviewBoardController implements ReviewBoardControllerDocs {
@@ -26,7 +28,7 @@ public class ReviewBoardController implements ReviewBoardControllerDocs {
     @LoginCheck
     @PostMapping("/write/{memberId}")
     public ResponseEntity<ReviewBoardResponse> writeReview(@PathVariable final Long memberId, @RequestBody final ReviewBoardRequest reviewBoardRequest) {
-
+        log.info("memberId : {}가 작성한 리뷰 게시글", memberId);
         return ResponseEntity.ok(reviewBoardService.writeReview(memberId, reviewBoardRequest));
     }
 
@@ -35,42 +37,46 @@ public class ReviewBoardController implements ReviewBoardControllerDocs {
     @LoginCheck
     @PatchMapping("/update")
     public ResponseEntity<ReviewBoardResponse> updatePost(@RequestBody final PostUpdateRequest postUpdateRequest) {
-
+        log.info("memberId : {}가 수정한 리뷰 게시글 : {}", postUpdateRequest.getMemberId(), postUpdateRequest.getBoardId());
         return ResponseEntity.ok().body(reviewBoardService.modifyReviewTitleAndContent(postUpdateRequest));
     }
 
     @LoginCheck
     @PatchMapping("/update-title")
     public ResponseEntity<ReviewBoardResponse> updateTitle(@RequestBody final PostUpdateRequest postUpdateRequest) {
+        log.info("memberId : {}가 제목만 수정한 리뷰 게시글 : {}", postUpdateRequest.getMemberId(), postUpdateRequest.getBoardId());
         return ResponseEntity.ok().body(reviewBoardService.modifyReviewTitle(postUpdateRequest));
     }
 
     @LoginCheck
     @PatchMapping("/update-content")
     public ResponseEntity<ReviewBoardResponse> updateContent(@RequestBody final PostUpdateRequest postUpdateRequest) {
+        log.info("memberId : {}가 내용만 수정한 리뷰 게시글 : {}", postUpdateRequest.getMemberId(), postUpdateRequest.getBoardId());
         return ResponseEntity.ok().body(reviewBoardService.modifyReviewContent(postUpdateRequest));
     }
 
     @LoginCheck
     @DeleteMapping("/delete-post")
     public ResponseEntity<Long> deletePost(@RequestBody final PostDeleteRequest postDeleteRequest) {
-
+        log.info("memberId : {} 가 삭제한 리뷰 게시글 : {}", postDeleteRequest.getMemberId(), postDeleteRequest.getBoardId());
         return ResponseEntity.ok(reviewBoardService.deleteReviewPost(postDeleteRequest));
     }
 
     @GetMapping("/show-post/{boardId}")
     public ResponseEntity<ReviewBoardResponse> showPost(@PathVariable final Long boardId) {
-
+        log.info("게시글 : {}를 조회한다", boardId);
         return ResponseEntity.ok(reviewBoardService.showPost(boardId));
     }
 
     @GetMapping("/show-searched-posts")
     public ResponseEntity<List<PerfumeReviewBoard>> showSearchedPosts(@RequestParam final String content) {
+        log.info("{} 이 포함된 게시글들을 조회하다.", content);
         return ResponseEntity.ok(reviewBoardService.showSearchedPosts(content));
     }
 
     @GetMapping("/show-my-posts/{memberId}")
     public ResponseEntity<List<PerfumeReviewBoard>> showMyPosts(@PathVariable final Long memberId) {
+        log.info("memberId : {} 가 작성한 리뷰 게시글만 조회한다", memberId);
         return ResponseEntity.ok(reviewBoardService.showMyReviewPost(memberId));
     }
 
