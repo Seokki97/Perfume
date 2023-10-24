4339fb7ba45c7aaa4da6b66dd2865511817686fa
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-24 20:43:00 +0900

    refactor - ReviewAnalyzeController Logging 적용

--- src/main/java/com/example/perfume/review/controller/ReviewAnalyzeController.java
+++ src/main/java/com/example/perfume/review/controller/ReviewAnalyzeController.java
@@ -3,6 +3,7 @@ package com.example.perfume.review.controller;
 import com.example.perfume.review.controller.docs.ReviewAnalyzeControllerDocs;
 import com.example.perfume.review.domain.review.PerfumeReviewBoard;
 import com.example.perfume.review.service.ReviewAnalyzeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/board")
 public class ReviewAnalyzeController implements ReviewAnalyzeControllerDocs {
@@ -23,16 +25,20 @@ public class ReviewAnalyzeController implements ReviewAnalyzeControllerDocs {
     @GetMapping("/show-like-ranking")
     public ResponseEntity<List<PerfumeReviewBoard>> showMostLikedPerfume(@RequestParam(value = "perfumeName", required = false) String perfumeName) {
         if (perfumeName == null) {
+            log.info("리뷰수에 따른 순위를 조회한다.");
             return ResponseEntity.ok(reviewAnalyzeService.sortLikeReviews());
         }
+        log.info("향수 이름 : {} 에 대한 순위를 조회한다.", perfumeName);
         return ResponseEntity.ok(reviewAnalyzeService.sortLikeReviewsFromSelectedPerfume(perfumeName));
     }
 
     @GetMapping("/show-unlike-ranking")
     public ResponseEntity<List<PerfumeReviewBoard>> showMostUnlikedPerfume(@RequestParam(value = "perfumeName", required = false) String perfumeName) {
         if (perfumeName == null) {
+            log.info("리뷰수에 따른 순위를 조회한다.");
             return ResponseEntity.ok(reviewAnalyzeService.sortUnlikeReviews());
         }
+        log.info("향수 이름 : {} 에 대한 순위를 조회한다.", perfumeName);
         return ResponseEntity.ok(reviewAnalyzeService.sortUnlikeReviewsFromSelectedPerfume(perfumeName));
     }
 
