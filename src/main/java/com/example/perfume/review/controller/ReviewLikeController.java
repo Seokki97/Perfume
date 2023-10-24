958cc44441b133e0fe8aafc70678b44460a50fdd
JunSeok Kim <skaksdl1238@gmail.com>
2023-10-24 20:43:58 +0900

    refactor - ReviewLikeController Logging 적용

--- src/main/java/com/example/perfume/review/controller/ReviewLikeController.java
+++ src/main/java/com/example/perfume/review/controller/ReviewLikeController.java
@@ -4,11 +4,13 @@ import com.example.perfume.member.service.jwt.LoginCheck;
 import com.example.perfume.review.controller.docs.ReviewLikeControllerDocs;
 import com.example.perfume.review.dto.like.ReviewLikeRequest;
 import com.example.perfume.review.service.ReviewLikeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/board")
 public class ReviewLikeController implements ReviewLikeControllerDocs {
@@ -23,7 +25,7 @@ public class ReviewLikeController implements ReviewLikeControllerDocs {
     @PatchMapping("/like")
     public ResponseEntity<Void> likePost(@RequestBody ReviewLikeRequest reviewLikeRequest) {
         reviewLikeService.likePost(reviewLikeRequest);
-
+        log.info("리뷰 게시글 {}의 상태를 {}로 변경하다.", reviewLikeRequest.getPost().getBoardId(), reviewLikeRequest.getLikeStatus());
         return ResponseEntity.noContent().build();
     }
 
@@ -31,7 +33,7 @@ public class ReviewLikeController implements ReviewLikeControllerDocs {
     @PatchMapping("/unlike")
     public ResponseEntity<Void> unlikePost(@RequestBody ReviewLikeRequest reviewLikeRequest) {
         reviewLikeService.unlikePost(reviewLikeRequest);
-
+        log.info("리뷰 게시글 {}의 상태를 {}로 변경하다.", reviewLikeRequest.getPost().getBoardId(), reviewLikeRequest.getLikeStatus());
         return ResponseEntity.noContent().build();
     }
 
