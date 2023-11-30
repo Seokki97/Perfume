20484d428883f5f0e55fd45a3a4a66641b4de5dd
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-30 11:24:21 +0900

    test - Claim 추출 테스트

--- src/test/java/com/example/perfume/member/jwt/JwtProviderTest.java
+++ src/test/java/com/example/perfume/member/jwt/JwtProviderTest.java
@@ -1,7 +1,10 @@
 package com.example.perfume.member.jwt;
 
+import com.example.perfume.member.exception.TokenInvalidException;
 import com.example.perfume.member.service.jwt.JwtProvider;
 import com.example.perfume.review.repository.ReviewBoardRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -73,4 +76,18 @@ public class JwtProviderTest {
         Assertions.assertEquals(refreshToken, jwtProvider.resolveRefreshToken(httpServletRequest));
     }
 
+    @DisplayName("AccessToken의 Claim을 추출한다. 정보가 다를 경우 Exception이 발생한다.")
+    @Test
+    void getClaims() {
+        String userPk = "123";
+        String accessToken = jwtProvider.generateAccessToken(userPk);
+        String fakeToken = "dosaodasoa";
+
+        Jws<Claims> claim = jwtProvider.getClaims(accessToken);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(userPk, claim.getBody().getSubject()),
+                () -> Assertions.assertEquals("https://inhyang.netlify.app/", claim.getBody().getIssuer()),
+                () -> Assertions.assertThrows(TokenInvalidException.class, () -> jwtProvider.getClaims(fakeToken))
+        );
+    }
 }
