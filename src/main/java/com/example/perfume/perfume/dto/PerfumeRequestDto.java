519451eaa5878d425524717f8b5c8308dd293e90
JunSeok Kim <skaksdl1238@gmail.com>
2023-12-21 01:16:16 +0900

    feat - Schema 생성

--- src/main/java/com/example/perfume/perfume/dto/PerfumeRequestDto.java
+++ src/main/java/com/example/perfume/perfume/dto/PerfumeRequestDto.java
@@ -2,15 +2,22 @@ package com.example.perfume.perfume.dto;
 
 
 import com.example.perfume.perfume.domain.Perfume;
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
 public class PerfumeRequestDto {
 
+    @Schema(description = "향수 id")
     private Long perfumeId;
+    @Schema(description = "향수 이름")
     private String perfumeName;
+    @Schema(description = "브랜드 이름")
     private String brandName;
+    @Schema(description = "향수 특징")
     private String perfumeFeature;
+    @Schema(description = "향수이미지 url")
     private String perfumeImageUrl;
 
     public PerfumeRequestDto() {
@@ -18,7 +25,8 @@ public class PerfumeRequestDto {
     }
 
     @Builder
-    public PerfumeRequestDto(Long perfumeId, String perfumeName, String brandName, String perfumeFeature, String perfumeImageUrl) {
+    public PerfumeRequestDto(Long perfumeId, String perfumeName, String brandName, String perfumeFeature,
+                             String perfumeImageUrl) {
         this.perfumeId = perfumeId;
         this.perfumeName = perfumeName;
         this.brandName = brandName;
