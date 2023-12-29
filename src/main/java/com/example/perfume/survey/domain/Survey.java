e72ee4294c186572b0f9a8c2790c290712ed042a
JunSeok Kim <skaksdl1238@gmail.com>
2023-12-29 11:47:31 +0900

    feat - Embedded 적용

--- src/main/java/com/example/perfume/survey/domain/Survey.java
+++ src/main/java/com/example/perfume/survey/domain/Survey.java
@@ -1,14 +1,15 @@
 package com.example.perfume.survey.domain;
 
 import com.example.perfume.perfume.domain.Perfume;
-import com.sun.istack.NotNull;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,41 +25,32 @@ public class Survey {
     private Long surveyId;
 
     @NotNull
-    @Column(nullable = false, length = 10)
-    private String genderAnswer;
-
-    @NotNull
-    @Column(nullable = false, length = 10)
-    private String scentAnswer;
-
-    @NotNull
-    @Column(nullable = false, length = 10)
-    private String moodAnswer;
-
-    @NotNull
-    @Column(nullable = false, length = 10)
-    private String seasonAnswer;
-
-    @NotNull
-    @Column(nullable = false, length = 30)
-    private String styleAnswer;
+    @Embedded
+    private Question question;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Perfume perfume;
 
     @Builder
-    public Survey(Long surveyId, String genderAnswer, String scentAnswer, String moodAnswer, String seasonAnswer,
-                  String styleAnswer, Perfume perfume) {
+    public Survey(Long surveyId, Question question, Perfume perfume) {
         this.surveyId = surveyId;
-        this.genderAnswer = genderAnswer;
-        this.scentAnswer = scentAnswer;
-        this.moodAnswer = moodAnswer;
-        this.seasonAnswer = seasonAnswer;
-        this.styleAnswer = styleAnswer;
+        this.question = question;
         this.perfume = perfume;
     }
 
     public String getMaintenance() {
         return perfume.getMaintenance();
+    }
+
+    public String getScentAnswer() {
+        return question.getScentAnswer();
+    }
+
+    public String getMoodAnswer() {
+        return question.getMoodAnswer();
+    }
+
+    public String getGenderAnswer() {
+        return question.getGenderAnswer();
     }
 }
