28631aedbba8e51adf2d9d1b9bf2c36fb5572b29
JunSeok Kim <skaksdl1238@gmail.com>
2023-12-29 13:01:38 +0900

    refactor - ReviewBoard Member 엔티티 LazyLoading으로 변경

--- src/main/java/com/example/perfume/review/domain/review/PerfumeReviewBoard.java
+++ src/main/java/com/example/perfume/review/domain/review/PerfumeReviewBoard.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -38,7 +39,7 @@ public class PerfumeReviewBoard {
     private LocalDateTime createdDateTme;
 
     @NotNull
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member writer;
 
     @NotNull
