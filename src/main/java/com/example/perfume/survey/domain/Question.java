8613956625ec46d71650f60c975e77440e21b528
JunSeok Kim <skaksdl1238@gmail.com>
2023-12-29 11:12:21 +0900

    feat - 임베디드 타입 생성

---
+++ src/main/java/com/example/perfume/survey/domain/Question.java
@@ -0,0 +1,34 @@
+package com.example.perfume.survey.domain;
+
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Question {
+
+    private String genderAnswer;
+
+    private String scentAnswer;
+
+    private String moodAnswer;
+
+    private String seasonAnswer;
+
+    private String styleAnswer;
+
+    @Builder
+    public Question(String genderAnswer, String scentAnswer, String moodAnswer, String seasonAnswer,
+                    String styleAnswer) {
+        this.genderAnswer = genderAnswer;
+        this.scentAnswer = scentAnswer;
+        this.moodAnswer = moodAnswer;
+        this.seasonAnswer = seasonAnswer;
+        this.styleAnswer = styleAnswer;
+    }
+}
