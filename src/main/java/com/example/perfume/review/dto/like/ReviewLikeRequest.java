1d6123d291670aeaf32e32acad146c4fdb5b2b26
JunSeok Kim <skaksdl1238@gmail.com>
2024-01-17 15:13:33 +0900

    feat - 요청 Dto에 LikeStatus 추가

--- src/main/java/com/example/perfume/review/dto/like/ReviewLikeRequest.java
+++ src/main/java/com/example/perfume/review/dto/like/ReviewLikeRequest.java
@@ -1,5 +1,6 @@
 package com.example.perfume.review.dto.like;
 
+import com.example.perfume.review.domain.like.LikeStatus;
 import lombok.Getter;
 
 @Getter
@@ -9,11 +10,14 @@ public class ReviewLikeRequest {
 
     private Long postId;
 
+    private LikeStatus likeStatus;
+
     public ReviewLikeRequest() {
     }
 
-    public ReviewLikeRequest(Long memberId, Long postId) {
+    public ReviewLikeRequest(Long memberId, Long postId, LikeStatus likeStatus) {
         this.memberId = memberId;
         this.postId = postId;
+        this.likeStatus = likeStatus;
     }
 }
