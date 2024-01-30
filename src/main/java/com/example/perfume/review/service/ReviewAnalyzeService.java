e007fe1f2ce68d55c37df8176d31e4442c243dc2
JunSeok Kim <skaksdl1238@gmail.com>
2024-01-30 17:57:06 +0900

    refactor - 기존 조회 로직 제거

--- src/main/java/com/example/perfume/review/service/ReviewAnalyzeService.java
+++ src/main/java/com/example/perfume/review/service/ReviewAnalyzeService.java
@@ -4,21 +4,15 @@ import com.example.perfume.review.domain.review.PerfumeReviewBoard;
 import com.example.perfume.review.dto.review.responseDto.ReviewAnalyzeResponse;
 import com.example.perfume.review.repository.ReviewBoardRepository;
 import java.util.List;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewAnalyzeService {
-
-    private static final String LIKE_COUNT = "likeCount";
-    private static final String UNLIKE_COUNT = "unlikeCount";
-
     private final ReviewBoardRepository reviewBoardRepository;
 
     public ReviewAnalyzeService(ReviewBoardRepository reviewBoardRepository) {
         this.reviewBoardRepository = reviewBoardRepository;
     }
-
 
     public ReviewAnalyzeResponse sortLikeReviews() {
         return new ReviewAnalyzeResponse(
@@ -28,17 +22,5 @@ public class ReviewAnalyzeService {
     public List<PerfumeReviewBoard> sortUnlikeReviews() {
 
         return reviewBoardRepository.findByUnLikeCountDesc();
-    }
-
-    public List<PerfumeReviewBoard> sortLikeReviewsFromSelectedPerfume(String perfumeName) {
-        Sort sort = Sort.by(Sort.Direction.DESC, LIKE_COUNT);
-
-        return reviewBoardRepository.findAllByTitleContainingOrContentContaining(perfumeName, perfumeName, sort);
-    }
-
-    public List<PerfumeReviewBoard> sortUnlikeReviewsFromSelectedPerfume(String perfumeName) {
-        Sort sort = Sort.by(Sort.Direction.DESC, UNLIKE_COUNT);
-
-        return reviewBoardRepository.findAllByTitleContainingOrContentContaining(perfumeName, perfumeName, sort);
     }
 }
