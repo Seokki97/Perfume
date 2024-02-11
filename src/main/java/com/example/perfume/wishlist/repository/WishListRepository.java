5a6725ea8a6a72967738185ae461175daaf25794
JunSeok Kim <skaksdl1238@gmail.com>
2024-02-11 20:29:48 +0900

    refactor - 삭제 메서드명 변경

--- src/main/java/com/example/perfume/wishlist/repository/WishListRepository.java
+++ src/main/java/com/example/perfume/wishlist/repository/WishListRepository.java
@@ -1,20 +1,17 @@
 package com.example.perfume.wishlist.repository;
 
 import com.example.perfume.wishlist.domain.WishList;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
     List<WishList> findByMember(Long memberId);
 
-    void deleteByMember_MemberId(Long memberId);
+    void deleteByMemberMemberId(Long memberId);
 
-    void deleteByMember_MemberIdAndPerfume_PerfumeId(Long memberId, Long perfumeId);
-
-    boolean existsByMember_MemberIdAndPerfume_PerfumeId(Long memberId, Long perfumeId);
+    void deleteByMemberMemberIdAndPerfumePerfumeId(Long memberId, Long perfumeId);
 
     List<WishList> findAll();
 }
