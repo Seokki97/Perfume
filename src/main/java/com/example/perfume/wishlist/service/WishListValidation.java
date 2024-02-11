6be7692772bbf49d4af9d169c94b71e2426afc3e
JunSeok Kim <skaksdl1238@gmail.com>
2024-02-11 20:28:42 +0900

    refactor - WishListUtil 삭제 -> 검증클래스로 변경

--- src/main/java/com/example/perfume/wishlist/service/WishListValidation.java
+++ src/main/java/com/example/perfume/wishlist/service/WishListValidation.java
@@ -1,30 +1,20 @@
 package com.example.perfume.wishlist.service;
 
-import com.example.perfume.wishlist.domain.WishList;
 import com.example.perfume.wishlist.dto.WishListRequest;
 import com.example.perfume.wishlist.repository.WishListRepository;
-import java.util.List;
 import java.util.Objects;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
-public class WishListUtil {
+public class WishListValidation {
 
     private static final int MAX_WISH_SIZE = 14;
 
     private final WishListRepository wishListRepository;
 
-    public WishListUtil(WishListRepository wishListRepository) {
+    public WishListValidation(WishListRepository wishListRepository) {
         this.wishListRepository = wishListRepository;
-    }
-
-    public void savePerfumeToWishList(WishList wishList) {
-        wishListRepository.save(wishList);
-    }
-
-    public List<WishList> showWishList(Long memberId) {
-        return wishListRepository.findByMember(memberId);
     }
 
     public boolean isEmptyWishList(Long memberId) {
@@ -39,16 +29,5 @@ public class WishListUtil {
 
     public boolean isWishListOverMaxSize(WishListRequest wishListRequest) {
         return wishListRepository.findByMember(wishListRequest.getMemberId()).size() > MAX_WISH_SIZE;
-    }
-
-    @Transactional
-    public void deleteAllWishedList(Long memberId) {
-        wishListRepository.deleteByMember_MemberId(memberId);
-    }
-
-    @Transactional
-    public void deleteSelectedWishElement(WishListRequest wishListRequest) {
-        wishListRepository.deleteByMember_MemberIdAndPerfume_PerfumeId(wishListRequest.getMemberId(),
-                wishListRequest.getPerfumeId());
     }
 }
